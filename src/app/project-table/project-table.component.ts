a54f773bd125b7c97a049e65a2bce0ccfa11a883
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-12 19:54:15 +0200

    add project table component

---
+++ src/app/project-table/project-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mvtool-project-table',
+  templateUrl: './project-table.component.html',
+  styleUrls: ['./project-table.component.css']
+})
+export class ProjectTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
