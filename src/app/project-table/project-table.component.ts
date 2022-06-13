217c36c049637adcd5145e38477a09861ae705d9
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-13 19:51:45 +0200

    list dummy projects

--- src/app/project-table/project-table.component.ts
+++ src/app/project-table/project-table.component.ts
@@ -6,6 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-table.component.css']
 })
 export class ProjectTableComponent implements OnInit {
+  projects: any[] = [
+    {
+      "description": "Some description of the test project.",
+      "jira_project_id": null,
+      "id": 1,
+      "name": "A test project"
+    },
+    {
+      "description": "Some description of the test project.",
+      "jira_project_id": null,
+      "id": 2,
+      "name": "A test project"
+    },
+    {
+      "description": "Some description of the test project.",
+      "jira_project_id": null,
+      "id": 3,
+      "name": "A test project"
+    },
+    {
+      "description": "Some description of the test project.",
+      "jira_project_id": null,
+      "id": 4,
+      "name": "A test project"
+    },
+    {
+      "description": "Some description of the test project.",
+      "jira_project_id": null,
+      "id": 5,
+      "name": "A test project"
+    }
+  ];
 
   constructor() { }
 
