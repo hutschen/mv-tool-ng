d4e18724805e0801a2719c04e37b5608e47608b7
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-17 21:07:50 +0200

    move crud service to its own file

---
+++ src/app/shared/crud.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { firstValueFrom } from "rxjs";
+import { environment } from "src/environments/environment";
+import { AuthService } from "./services/auth.service";
+
+export abstract class CRUDService<InputType, OutputType> {
+    protected _itemsUrl: string;
+  
+    constructor(
+          protected _httpClient: HttpClient, protected _auth: AuthService,
+          relativeBaseUrl: string) {
+        this._itemsUrl = `${environment.baseUrl}/${relativeBaseUrl}`
+      }
+  
+      protected _getItemUrl(itemId: number): string {
+        return `${this._itemsUrl}/${itemId}`
+      }
+  
+      protected get _httpOptions(): object {
+        const credentials = this._auth.credentials
+        const credentials_str = `${credentials.username}:${credentials.password}`
+        return {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + btoa(credentials_str)
+          })
+        }
+      }
+  
+      async list(): Promise<OutputType[]> {
+        const request$ = this._httpClient.get<OutputType[]>(
+          this._itemsUrl, this._httpOptions)
+        return firstValueFrom(request$)
+      }
+  
+      async create(item: InputType): Promise<OutputType> {
+        const request$ = this._httpClient.post<OutputType>(
+          this._itemsUrl, item, this._httpOptions)
+        return firstValueFrom(request$)
+      }
+  
+      async get(itemId: number) : Promise<OutputType> {
+        const request$ = this._httpClient.get<OutputType>(
+          this._getItemUrl(itemId), this._httpOptions)
+        return firstValueFrom(request$)
+      }
+  
+      async update(
+          itemIt: number, item: InputType) : Promise<OutputType> {
+        const request$ = this._httpClient.put<OutputType>(
+          this._getItemUrl(itemIt), item, this._httpOptions)
+        return firstValueFrom(request$)
+      }
+  
+      async delete(itemId: number) {
+        const request$ = this._httpClient.delete<null>(
+          this._getItemUrl(itemId), this._httpOptions)
+        return firstValueFrom(request$)
+      }
+  }
--- src/app/shared/services/project.service.ts
+++ src/app/shared/services/project.service.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { CRUDService } from '../crud.service';
 import { AuthService } from './auth.service';
 
 export interface IProjectInput {
@@ -12,62 +11,6 @@ export interface IProjectInput {
 
 export interface IProject extends IProjectInput {
   id: number;
-}
-
-export abstract class CRUDService<InputType, OutputType> {
-  protected _itemsUrl: string;
-
-  constructor(
-        protected _httpClient: HttpClient, protected _auth: AuthService,
-        relativeBaseUrl: string) {
-      this._itemsUrl = `${environment.baseUrl}/${relativeBaseUrl}`
-    }
-
-    protected _getItemUrl(itemId: number): string {
-      return `${this._itemsUrl}/${itemId}`
-    }
-
-    protected get _httpOptions(): object {
-      const credentials = this._auth.credentials
-      const credentials_str = `${credentials.username}:${credentials.password}`
-      return {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic ' + btoa(credentials_str)
-        })
-      }
-    }
-
-    async list(): Promise<OutputType[]> {
-      const request$ = this._httpClient.get<OutputType[]>(
-        this._itemsUrl, this._httpOptions)
-      return firstValueFrom(request$)
-    }
-
-    async create(item: InputType): Promise<OutputType> {
-      const request$ = this._httpClient.post<OutputType>(
-        this._itemsUrl, item, this._httpOptions)
-      return firstValueFrom(request$)
-    }
-
-    async get(itemId: number) : Promise<OutputType> {
-      const request$ = this._httpClient.get<OutputType>(
-        this._getItemUrl(itemId), this._httpOptions)
-      return firstValueFrom(request$)
-    }
-
-    async update(
-        itemIt: number, item: InputType) : Promise<OutputType> {
-      const request$ = this._httpClient.put<OutputType>(
-        this._getItemUrl(itemIt), item, this._httpOptions)
-      return firstValueFrom(request$)
-    }
-
-    async delete(itemId: number) {
-      const request$ = this._httpClient.delete<null>(
-        this._getItemUrl(itemId), this._httpOptions)
-      return firstValueFrom(request$)
-    }
 }
 
 @Injectable({
