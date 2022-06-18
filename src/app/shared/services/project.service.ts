305e91a317ee62d9bd0aab85010c95b7d8454783
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-18 14:26:01 +0200

    rename methods in project service

--- src/app/shared/services/project.service.ts
+++ src/app/shared/services/project.service.ts
@@ -25,23 +25,23 @@ export class ProjectService {
     return `${this.getProjectsUrl()}/${projectId}`
   }
 
-  async list(): Promise<IProject[]> {
+  async listProjects(): Promise<IProject[]> {
     return this._crud.list(this.getProjectsUrl())
   }
 
-  async create(projectInput: IProjectInput): Promise<IProject> {
+  async createProject(projectInput: IProjectInput): Promise<IProject> {
     return this._crud.create(this.getProjectsUrl(), projectInput)
   }
 
-  async read(projectId: number): Promise<IProject> {
+  async getProject(projectId: number): Promise<IProject> {
     return this._crud.read(this.getProjectUrl(projectId))
   }
 
-  async update(projectId: number, projectInput: IProjectInput): Promise<IProject> {
+  async updateProject(projectId: number, projectInput: IProjectInput): Promise<IProject> {
     return this._crud.update(this.getProjectUrl(projectId), projectInput)
   }
 
-  async delete(projectId: number): Promise<null> {
+  async deleteProject(projectId: number): Promise<null> {
     return this._crud.delete(this.getProjectUrl(projectId))
   }
 }
