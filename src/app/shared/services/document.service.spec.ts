872aee9f9c67b95ff07bbf7e3f611776a2f6efa7
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-19 11:38:01 +0200

    disable unimplemented tests

--- src/app/shared/services/document.service.spec.ts
+++ src/app/shared/services/document.service.spec.ts
@@ -10,7 +10,7 @@ describe('DocumentService', () => {
     service = TestBed.inject(DocumentService);
   });
 
-  it('should be created', () => {
+  xit('should be created', () => {
     expect(service).toBeTruthy();
   });
 });
