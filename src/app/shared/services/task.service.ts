e5e63c1650192b6f35b72c944604fed5dfa8843f
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-22 21:12:47 +0200

    implement task service

--- src/app/shared/services/task.service.ts
+++ src/app/shared/services/task.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { CRUDService } from './crud.service';
+import { MeasureService } from './measure.service';
+
+export interface ITaskInput {}
+export interface ITask extends ITaskInput {}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(
+    protected _crud: CRUDService<ITaskInput, ITask>,
+    protected _measures: MeasureService) {}
+
+  getTasksUrl(measureId: number): string {
+    return `${this._measures.getMeasureUrl(measureId)}/tasks`
+  }
+
+  getTaskUrl(taskId: number): string {
+    return `tasks/${taskId}`
+  }
+
+  async listTasks(measureId: number): Promise<ITask[]> {
+    return this._crud.list(this.getTasksUrl(measureId))
+  }
+
+  async createTask(measureId: number, taskInput: ITaskInput): Promise<ITask> {
+    return this._crud.create(this.getTasksUrl(measureId), taskInput)
+  }
+
+  async getTask(taskId: number): Promise<ITask> {
+    return this._crud.read(this.getTaskUrl(taskId))
+  }
+
+  async updateTask(taskId: number, taskInput: ITaskInput): Promise<ITask> {
+    return this._crud.update(this.getTaskUrl(taskId), taskInput)
+  }
+
+  async deleteTask(taskId: number): Promise<null> {
+    return this._crud.delete(this.getTaskUrl(taskId))
+  }
 }
