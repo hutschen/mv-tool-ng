0065095be8e2dc5a154298aaacfc6063e16b8a9d
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-25 17:01:43 +0200

    add user service

---
+++ src/app/shared/services/user.service.spec.ts
@@ -0,0 +1,51 @@
+import { TestBed } from '@angular/core/testing';
+import { 
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { CRUDService } from './crud.service';
+import { UserService, IUser } from './user.service';
+
+describe('UserService', () => {
+  let sut: UserService;
+  let crud: CRUDService<IUser, IUser>
+  let httpMock: HttpTestingController
+  let outputMock: IUser
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    sut = TestBed.inject(UserService);
+    crud = TestBed.inject(CRUDService)
+    httpMock = TestBed.inject(HttpTestingController)
+    outputMock = {
+      display_name: 'Firstname Lastname',
+      email_address: 'firstname.lastname@domain'
+    }
+  });
+
+  afterEach(() => {
+    httpMock.verify()
+  })
+
+  it('should be created', () => {
+    expect(sut).toBeTruthy();
+  });
+
+  it('should return user url', () => {
+    expect(sut.getUserUrl()).toEqual('jira/user')
+  })
+
+  it('should get user', (done: DoneFn) => {
+    sut.getUser().then((value) => {
+      expect(value).toEqual(outputMock)
+      done()
+    })
+    const mockResponse = httpMock.expectOne({
+      method: 'get',
+      url: crud.toAbsoluteUrl(sut.getUserUrl())
+    })
+    mockResponse.flush(outputMock)
+  })
+});
---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CRUDService } from './crud.service';
+
+export interface IUser {
+  display_name: string
+  email_address: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(protected _crud: CRUDService<IUser, IUser>) {}
+
+  getUserUrl() : string {
+    return 'jira/user'
+  }
+
+  async getUser(): Promise<IUser> {
+    return this._crud.read(this.getUserUrl())
+  }
+}
