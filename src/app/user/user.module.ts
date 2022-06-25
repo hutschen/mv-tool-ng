3bdd47de29f0870cb9a3014956750fff7edfd021
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-25 15:07:12 +0200

    rename auth to user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
@@ -11,4 +9,4 @@ import { MaterialModule } from '../material/material.module';
     MaterialModule,
   ]
 })
-export class AuthModule { }
+export class UserModule { }
