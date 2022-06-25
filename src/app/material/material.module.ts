e13ee854bd5babd4f1480de1d18fcf8c917b6650
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-25 12:46:01 +0200

    import flex layout

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,6 +13,7 @@ import { MatTableModule } from '@angular/material/table';
   exports: [
     FormsModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatIconModule,
     MatTableModule,
