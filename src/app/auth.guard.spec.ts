fbe1b4b43fedf424880ba8bc0023fadebcccded8
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-26 17:22:42 +0200

    implement auth guard and corresponding test

--- src/app/auth.guard.spec.ts
+++ src/app/auth.guard.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
+import { AuthService, ICredentials } from './shared/services/auth.service';
 
 describe('AuthGuard', () => {
-  let guard: AuthGuard;
+  let sut: AuthGuard;
+  let auth: AuthService;
+  let credentials: ICredentials;
+  let routeMock: ActivatedRouteSnapshot;
+  let stateMock: RouterStateSnapshot;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard);
+    auth = TestBed.inject(AuthService);
+    sut = TestBed.inject(AuthGuard);
+    credentials = {username: 'test', password: 'test'}
+    routeMock = {} as ActivatedRouteSnapshot
+    stateMock = {url: 'test'} as RouterStateSnapshot
+
+    if (auth.isLoggedIn) {
+      auth.logOut()
+      expect(auth.isLoggedIn).toBeFalse()
+    }
   });
 
   it('should be created', () => {
-    expect(guard).toBeTruthy();
+    expect(sut).toBeTruthy();
   });
+
+  it('should recognize logged in user', () => {
+    auth.logIn(credentials)
+    const result = sut.canActivate(routeMock, stateMock)
+    expect(result).toEqual(auth.isLoggedIn)
+    expect(result).toBeTrue()
+  });
+
+  it('should recognize logged out user', () => {
+    auth.logOut()
+    const result = sut.canActivate(routeMock, stateMock)
+    expect(result).toEqual(auth.isLoggedIn)
+    expect(result).toBeFalse()
+  });
+
 });
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { 
+  ActivatedRouteSnapshot, 
+  CanActivate, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './shared/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(protected _auth: AuthService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): boolean {
+    return this._auth.isLoggedIn;
   }
   
 }
