a20849d3bdad8e1a11c476e22c7d4818865df076
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-26 12:54:20 +0200

    add login form to login view

--- src/app/views/login-view.component.ts
+++ src/app/views/login-view.component.ts
@@ -2,8 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mvtool-login-view',
-  template: `<mvtool-user-login></mvtool-user-login>`,
+  template: `
+    <div class='container' fxLayout='row' fxLayoutAlign='center center'>
+      <mat-card>
+        <mvtool-user-login></mvtool-user-login>
+      </mat-card>
+    </div>
+  `,
   styles: [
+    '.container { padding: 20px;}'
   ]
 })
 export class LoginViewComponent implements OnInit {
