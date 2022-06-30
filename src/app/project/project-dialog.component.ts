30f57d8ab087e615aba529a44a7593f15a70ba16
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-06-30 20:17:43 +0200

    improve project dialog

--- src/app/project/project-dialog.component.ts
+++ src/app/project/project-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { IProjectInput } from '../shared/services/project.service';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { IJiraProject, JiraProjectService } from '../shared/services/jira-project.service';
+import { IProject, IProjectInput, ProjectService } from '../shared/services/project.service';
 
 @Component({
   selector: 'mvtool-project-dialog',
@@ -8,11 +9,16 @@ import { IProjectInput } from '../shared/services/project.service';
   styleUrls: ['./project-dialog.component.css']
 })
 export class ProjectDialogComponent implements OnInit {
+  jiraProjects: IJiraProject[] = [];
   projectInput: IProjectInput = {
     name: '',
   }
 
-  constructor(protected _dialogRef: MatDialogRef<ProjectDialogComponent>) { }
+  constructor(
+    protected _dialogRef: MatDialogRef<ProjectDialogComponent>, 
+    protected _projectService: ProjectService,
+    protected _jiraProjectService: JiraProjectService,
+    @Inject(MAT_DIALOG_DATA) protected _project: IProject | null) { }
 
   onSave(): void {
     this._dialogRef.close(this.projectInput);
@@ -21,7 +27,28 @@ export class ProjectDialogComponent implements OnInit {
     this._dialogRef.close(null);
   }
 
-  ngOnInit(): void {
+  get createMode(): boolean {
+    return this._project === null;
+  }
+
+  async ngOnInit() {
+    if (this._project) {
+      this.projectInput = {
+        name: this._project.name,
+        description: this._project.description,
+        jira_project_id: this._project.jira_project_id,
+      }
+    } else {
+      this.projectInput = {
+        name: '',
+        description: '',
+        jira_project_id: null,
+      }
+    }
+
+    if (!this.projectInput.jira_project_id) {
+      this.jiraProjects = await this._jiraProjectService.getJiraProjects();
+    }
   }
 
 }
