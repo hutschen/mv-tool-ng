afa65126731be2953483018ac425fb0827c170fc
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-03 10:40:33 +0200

    use Project instead of IProject in project table component
    refactor project table component

--- src/app/project/project-table.component.ts
+++ src/app/project/project-table.component.ts
@@ -1,28 +1,36 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { IProject, ProjectService } from '../shared/services/project.service';
+import { Project, ProjectService } from '../shared/services/project.service';
 import { ProjectDialogComponent } from './project-dialog.component';
 
 @Component({
   selector: 'mvtool-project-table',
   templateUrl: './project-table.component.html',
-  styleUrls: ['./project-table.component.css']
+  styles: []
 })
 export class ProjectTableComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['name', 'description', 'jira_project_id', 'options'];
-  dataSource: MatTableDataSource<IProject> = new MatTableDataSource<IProject>();
+  dataSource: MatTableDataSource<Project> = new MatTableDataSource<Project>();
   @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
   @ViewChild(MatSort) sort: MatSort | null = null;
 
-  constructor(protected _projectService: ProjectService, protected _dialog: MatDialog) {}
+  constructor(
+    protected _projectService: ProjectService, 
+    protected _dialog: MatDialog) {}
 
-  async uploadExcel(): Promise<void> {}
-  async downloadExcel(): Promise<void> {}
+  async ngOnInit() {
+    this.dataSource.data = await this._projectService.listProjects()
+  }
 
-  createProject() {
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  onCreateProject() {
     let dialogRef = this._dialog.open(ProjectDialogComponent, {
       width: '500px'
     })
@@ -34,12 +42,7 @@ export class ProjectTableComponent implements OnInit, AfterViewInit {
     })
   }
 
-  async deleteProject(project: IProject): Promise<void> {
-    await this._projectService.deleteProject(project.id)
-    this.dataSource.data = await this._projectService.listProjects()
-  }
-
-  editProject(project: IProject): void {
+  onEditProject(project: Project): void {
     let dialogRef = this._dialog.open(ProjectDialogComponent, {
       width: '500px',
       data: project,
@@ -52,18 +55,13 @@ export class ProjectTableComponent implements OnInit, AfterViewInit {
     })
   }
 
-  applyFilter(event: Event) { 
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  async ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-  
-  async ngOnInit(): Promise<void> {
+  async onDeleteProject(project: Project): Promise<void> {
+    await this._projectService.deleteProject(project.id)
     this.dataSource.data = await this._projectService.listProjects()
   }
 
+  onFilterProjects(event: Event) { 
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
