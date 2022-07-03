02aaa36312c4891093626e689e69a271c3a99a35
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-03 12:10:38 +0200

    add handler to navigate on requirement view

--- src/app/views/projects-view.component.ts
+++ src/app/views/projects-view.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Project } from '../shared/services/project.service';
 
 @Component({
   selector: 'mvtool-projects-view',
   template: `
-    <mvtool-project-table></mvtool-project-table>
+    <mvtool-project-table 
+      (projectClicked)="onProjectClicked($event)">
+    </mvtool-project-table>
   `,
   styles: [
   ]
 })
 export class ProjectsViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected _router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onProjectClicked(project: Project) {
+    this._router.navigate(['/projects', project.id, 'requirements']);
   }
 
 }
