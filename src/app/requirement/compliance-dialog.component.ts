5ff5272790d7c81b9d453a1602b9de565b29eea1
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-05 19:49:39 +0200

    logic for compliance dialog

--- src/app/requirement/compliance-dialog.component.ts
+++ src/app/requirement/compliance-dialog.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { IRequirement, IRequirementInput, Requirement, RequirementService } from '../shared/services/requirement.service';
 
 @Component({
   selector: 'mvtool-compliance-dialog',
   templateUrl: './compliance-dialog.component.html',
   styles: [
+    'textarea { min-height: 100px; }'
   ]
 })
-export class ComplianceDialogComponent implements OnInit {
+export class ComplianceDialogComponent {
+  requirementInput: IRequirementInput;
 
-  constructor() { }
+  constructor(
+    protected _dialogRef: MatDialogRef<ComplianceDialogComponent>, 
+    protected _requirementService: RequirementService,
+    @Inject(MAT_DIALOG_DATA) protected _requirement: Requirement) { 
+      this.requirementInput = this._requirement.toRequirementInput();
+    }
 
-  ngOnInit(): void {
+  onSave(form: NgForm): void {
+    if (form.valid) {
+      this._dialogRef.close(this.requirementInput);
+    }
   }
 
+  onCancel(): void {
+    this._dialogRef.close(null);
+  }
 }
