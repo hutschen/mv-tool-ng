c7e333388adcad0119ce1ba60f186b8fe17b2720
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-07 15:12:24 +0200

    disable tests for task table and dialog

--- src/app/task/task-dialog.component.spec.ts
+++ src/app/task/task-dialog.component.spec.ts
@@ -17,7 +17,7 @@ describe('TaskDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  xit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
