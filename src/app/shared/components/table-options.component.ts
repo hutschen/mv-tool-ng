807b46fbf246328b6e09fb02740afb1612d26416
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-07 20:31:47 +0200

    generate table options component

---
+++ src/app/shared/components/table-options.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mvtool-table-options',
+  template: `
+    <p>
+      table-options works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TableOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TruncatePipe } from './pipes';
 import { HttpClientModule } from '@angular/common/http';
+import { TableOptionsComponent } from './components/table-options.component';
 
 
 
 @NgModule({
   declarations: [
     TruncatePipe,
+    TableOptionsComponent,
   ],
   imports: [
     CommonModule,
