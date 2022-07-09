1dbc9cbb7d8b038acdc77f51ea652b52be60819c
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-09 09:30:13 +0200

    generate table component

---
+++ src/app/shared/components/table.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mvtool-table',
+  template: `
+    <p>
+      table works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { TableOptionsComponent } from './components/table-options.component';
 import { MaterialModule } from '../material/material.module';
 import { TableToolbarComponent } from './components/table-toolbar.component';
+import { TableComponent } from './components/table.component';
 
 
 
@@ -13,6 +14,7 @@ import { TableToolbarComponent } from './components/table-toolbar.component';
     TruncatePipe,
     TableOptionsComponent,
     TableToolbarComponent,
+    TableComponent,
   ],
   imports: [
     CommonModule,
