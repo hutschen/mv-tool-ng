2659c96b47528a7aae77665f5fdcc7664de24450
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-10 19:34:11 +0200

    use jira issue dialog in jira issue input

--- src/app/jira-issue/jira-issue-input.component.ts
+++ src/app/jira-issue/jira-issue-input.component.ts
@@ -1,25 +1,52 @@
-import { Component, Input } from '@angular/core';
-import { Measure } from '../shared/services/measure.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { IJiraIssueInput } from '../shared/services/jira-issue.service';
+import { IMeasureInput } from '../shared/services/measure.service';
+import { Project, ProjectService } from '../shared/services/project.service';
+import { JiraIssueDialogComponent } from './jira-issue-dialog.component';
 
 @Component({
   selector: 'mvtool-jira-issue-input',
   template: `
-    <div>
-      <button mat-button>
+    <span *ngIf="project?.hasPermissionOnJiraProject && project?.hasJiraProject">
+      <!-- Button to create jira issue -->
+      <button mat-button (click)="onCreateJiraIssue()">
         <mat-icon>add</mat-icon>
-        Create JIRA issue
+        Create JIRA Issue
       </button>
-    </div>
+    </span>
   `,
   styles: [
   ]
 })
-export class JiraIssueInputComponent {
-  @Input() measure: Measure | null = null
+export class JiraIssueInputComponent implements OnInit {
+  @Input() projectId: number | null = null;
+  @Input() measureInput: IMeasureInput | null = null;
+  @Output() onJiraIssueInput = new EventEmitter<IJiraIssueInput>();
+  project: Project | null = null;
 
-  constructor() { }
+  constructor(
+    protected _projectService: ProjectService,
+    protected _dialog: MatDialog) { }
 
-  createJiraIssue(): void {
+  async ngOnInit(): Promise<void> {
+    if(this.projectId) {
+      this.project = await this._projectService.getProject(this.projectId);
+    }
+  }
 
+  onCreateJiraIssue(): void {
+    let dialogRef = this._dialog.open(JiraIssueDialogComponent, {
+      width: '500px',
+      data: {
+        jiraProjectId: this.project?.jira_project_id,
+        measureInput: this.measureInput
+      }
+    })
+    dialogRef.afterClosed().subscribe(jiraIssueInput => {
+      if (jiraIssueInput) {
+        this.onJiraIssueInput.emit(jiraIssueInput);
+      }
+    })
   }
 }
