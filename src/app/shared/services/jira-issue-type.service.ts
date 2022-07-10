276589aa0e11e579fd4901ad26b63cc3a75689c1
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-10 11:17:41 +0200

    implement issue type service

--- src/app/shared/services/jira-issue-type.service.ts
+++ src/app/shared/services/jira-issue-type.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { CRUDService } from './crud.service';
+import { JiraProjectService } from './jira-project.service';
+
+export interface IJiraIssueType {
+  id: string
+  name: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class JiraIssueTypeService {
 
-  constructor() { }
+  constructor(
+    protected _crud: CRUDService<IJiraIssueType, IJiraIssueType>,
+    protected _projects: JiraProjectService) {}
+
+  getJiraIssueTypesUrl(jiraProjectId: string) : string {
+    return `${this._projects.getJiraProjectUrl(jiraProjectId)}/issuetypes`
+  }
+
+  async getJiraIssueTypes(jiraProjectId: string): Promise<IJiraIssueType[]> {
+    return this._crud.list(this.getJiraIssueTypesUrl(jiraProjectId))
+  }
 }
