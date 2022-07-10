fa401cc4497b749f13f1a79a455450c78a200a67
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-10 18:08:33 +0200

    rework jira issue label

--- src/app/jira-issue/jira-issue-label.component.ts
+++ src/app/jira-issue/jira-issue-label.component.ts
@@ -1,26 +1,24 @@
 import { Component, Input } from '@angular/core';
-import { IJiraIssue } from '../shared/services/jira-issue.service';
 import { Measure } from '../shared/services/measure.service';
-import { Project, ProjectService } from '../shared/services/project.service';
 
 @Component({
   selector: 'mvtool-jira-issue-label',
   template: `
-    <span *ngIf="measure_ && project">
-      <span *ngIf="project.hasPermissionOnJiraProject && measure_.jira_issue">
+    <span *ngIf="measure">
+      <span *ngIf="measure.hasPermissionOnJiraIssue && measure.jira_issue">
         <a mat-button
-          href="{{measure_.jira_issue.url}}" 
+          href="{{measure.jira_issue.url}}" 
           target="_blank" rel="noopener noreferrer">
           <mat-icon>open_in_new</mat-icon>
-          {{measure_.jira_issue.key | truncate }} / 
-          {{measure_.jira_issue.summary | truncate }}
-          <mat-icon *ngIf="measure_.jira_issue.status.completed">check</mat-icon>
-          <mat-icon *ngIf="!measure_.jira_issue.status.completed">close</mat-icon>
+          {{measure.jira_issue.key | truncate }} / 
+          {{measure.jira_issue.summary | truncate }}
+          <mat-icon *ngIf="measure.jira_issue.status.completed">check</mat-icon>
+          <mat-icon *ngIf="!measure.jira_issue.status.completed">close</mat-icon>
         </a>
       </span>
-      <span *ngIf="!project.hasPermissionOnJiraProject">
+      <span *ngIf="!measure.hasPermissionOnJiraIssue">
         <mat-icon 
-          matTooltip="You have not the permission to view the JIRA project">
+          matTooltip="You have not the permission to view the JIRA issue">
           block
         </mat-icon>
       </span>
@@ -29,23 +27,7 @@ import { Project, ProjectService } from '../shared/services/project.service';
   styles: []
 })
 export class JiraIssueLabelComponent {
-  project: Project | null = null;
-  measure_: Measure | null = null;
+  @Input() measure: Measure | null = null;
 
-  constructor(protected _projectService: ProjectService) { }
-
-  @Input() 
-  set measure(measure: Measure | null) {
-    this.measure_ = measure;
-    this.reload();
-  }
-
-  async reload(): Promise<void> {
-    if (this.measure_) {
-      // reload project because it is possibly incomplete
-      this.project = await this._projectService.getProject(
-        this.measure_.requirement.project.id);
-    }
-  }
-
+  constructor() { }
 }
