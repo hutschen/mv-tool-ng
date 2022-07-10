8e20d400944e41fecbf9cbc9ae262922a275c99e
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-10 14:05:30 +0200

    disable tests for jira issue input and label

--- src/app/jira-issue/jira-issue-label.component.spec.ts
+++ src/app/jira-issue/jira-issue-label.component.spec.ts
@@ -17,7 +17,7 @@ describe('JiraIssueLabelComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  xit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
