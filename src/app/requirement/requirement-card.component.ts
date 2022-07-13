2edecbb0bbf754a8b1f7fc08659b23b0e359117d
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-07-13 19:10:18 +0200

    implement and include requirement card

--- src/app/requirement/requirement-card.component.ts
+++ src/app/requirement/requirement-card.component.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Requirement } from '../shared/services/requirement.service';
 
 @Component({
   selector: 'mvtool-requirement-card',
   template: `
-    <p>
-      requirement-card works!
-    </p>
+    <div fxLayout="column" *ngIf="requirement">
+      <mat-card class="mat-elevation-z0">
+        <mat-card-title>
+          {{ requirement.reference | truncate:30 }}
+          {{ requirement.summary | truncate }}
+        </mat-card-title>
+        <mat-card-content *ngIf="hasContent">
+          <p>{{ requirement.description }}</p>
+          <p *ngIf="requirement.target_object">
+            <strong>Target object:</strong>
+            {{ requirement.target_object }}
+          </p>
+          <p *ngIf="requirement.compliance_status">
+            <strong>Compliance status:</strong>
+            {{ requirement.compliance_status }}
+          </p>
+          <p *ngIf="requirement.compliance_comment">
+            <strong>Compliance comment:</strong>
+            {{ requirement.compliance_comment }}
+          </p>
+        </mat-card-content>
+      </mat-card>
+    </div>
   `,
-  styles: [
-  ]
+  styles: []
 })
-export class RequirementCardComponent implements OnInit {
+export class RequirementCardComponent {
+  @Input() requirement: Requirement | null = null;
 
   constructor() { }
 
-  ngOnInit(): void {
+  get hasContent(): boolean {
+    if (this.requirement) {
+      return (
+        Boolean(this.requirement.description) || 
+        Boolean(this.requirement.target_object));
+    } else {
+      return false;
+    }
   }
-
 }
--- src/app/views/measure-view.component.ts
+++ src/app/views/measure-view.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Measure } from '../shared/services/measure.service';
-import { Requirement } from '../shared/services/requirement.service';
+import { Requirement, RequirementService } from '../shared/services/requirement.service';
 
 @Component({
   selector: 'mvtool-measure-view',
   template: `
-    <mvtool-measure-table
-      [requirementId]="requirementId">
-    </mvtool-measure-table>
+    <div *ngIf="requirement" fxLayout="column">
+      <mvtool-requirement-card [requirement]="requirement"></mvtool-requirement-card>
+      <mat-divider></mat-divider>
+      <mvtool-measure-table
+        [requirementId]="requirement.id">
+      </mvtool-measure-table>
+    </div>
   `,
   styles: [
   ]
 })
 export class MeasureViewComponent implements OnInit {
-  requirementId: number | null = null
+  requirement: Requirement | null = null;
 
   constructor(
     protected _route: ActivatedRoute,
-    protected _router: Router) { }
+    protected _router: Router,
+    protected _requirementService: RequirementService) { }
 
-  ngOnInit(): void {
-    this.requirementId = Number(this._route.snapshot.paramMap.get('requirementId'))
+  async ngOnInit(): Promise<void> {
+    const requirementId = Number(this._route.snapshot.paramMap.get('requirementId'))
+    this.requirement = await this._requirementService.getRequirement(requirementId);
   }
 }
