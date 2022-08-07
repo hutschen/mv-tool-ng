85f31574062ae2e36cd47f28916cf6cef6891114
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-07 15:11:01 +0200

    format compliance dialog

--- src/app/requirement/compliance-dialog.component.ts
+++ src/app/requirement/compliance-dialog.component.ts
@@ -1,28 +1,31 @@
 import { Component, Inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { IRequirementInput, Requirement, RequirementService } from '../shared/services/requirement.service';
+import {
+  IRequirementInput,
+  Requirement,
+  RequirementService,
+} from '../shared/services/requirement.service';
 
 @Component({
   selector: 'mvtool-compliance-dialog',
   templateUrl: './compliance-dialog.component.html',
-  styles: [
-    'textarea { min-height: 100px; }'
-  ]
+  styles: ['textarea { min-height: 100px; }'],
 })
 export class ComplianceDialogComponent {
-  complianceStates = ['C', 'PC', 'NC', 'N/A']
+  complianceStates = ['C', 'PC', 'NC', 'N/A'];
   requirementInput: IRequirementInput;
 
   constructor(
     protected _dialogRef: MatDialogRef<ComplianceDialogComponent>,
     protected _requirementService: RequirementService,
-    @Inject(MAT_DIALOG_DATA) protected _requirement: Requirement) {
+    @Inject(MAT_DIALOG_DATA) protected _requirement: Requirement
+  ) {
     this.requirementInput = this._requirement.toRequirementInput();
   }
 
   get complianceCommentDisabled(): boolean {
-    return this.requirementInput.compliance_status === null
+    return this.requirementInput.compliance_status === null;
   }
 
   set complianceStatus(value: string | null) {
@@ -33,7 +36,7 @@ export class ComplianceDialogComponent {
   }
 
   get complianceStatus(): string | null {
-    return this.requirementInput.compliance_status
+    return this.requirementInput.compliance_status;
   }
 
   onSave(form: NgForm): void {
