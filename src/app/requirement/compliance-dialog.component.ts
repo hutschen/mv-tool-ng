abf7ef633dc13865f738fd7416a26ac257560abc
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-07 14:34:57 +0200

    remove unused import

--- src/app/requirement/compliance-dialog.component.ts
+++ src/app/requirement/compliance-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { IRequirement, IRequirementInput, Requirement, RequirementService } from '../shared/services/requirement.service';
+import { IRequirementInput, Requirement, RequirementService } from '../shared/services/requirement.service';
 
 @Component({
   selector: 'mvtool-compliance-dialog',
@@ -15,11 +15,11 @@ export class ComplianceDialogComponent {
   requirementInput: IRequirementInput;
 
   constructor(
-    protected _dialogRef: MatDialogRef<ComplianceDialogComponent>, 
+    protected _dialogRef: MatDialogRef<ComplianceDialogComponent>,
     protected _requirementService: RequirementService,
-    @Inject(MAT_DIALOG_DATA) protected _requirement: Requirement) { 
-      this.requirementInput = this._requirement.toRequirementInput();
-    }
+    @Inject(MAT_DIALOG_DATA) protected _requirement: Requirement) {
+    this.requirementInput = this._requirement.toRequirementInput();
+  }
 
   get complianceCommentDisabled(): boolean {
     return this.requirementInput.compliance_status === null
