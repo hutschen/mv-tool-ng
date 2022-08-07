91a1e9b3e8eb8f110bd1456e87749c3606d7f807
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-07 17:28:43 +0200

    format requirement service

--- src/app/shared/services/requirement.service.ts
+++ src/app/shared/services/requirement.service.ts
@@ -3,17 +3,17 @@ import { CRUDService } from './crud.service';
 import { IProject, Project, ProjectService } from './project.service';
 
 export interface IRequirementInput {
-  reference: string | null
-  summary: string
-  description: string | null
-  target_object: string | null
-  compliance_status: string | null
-  compliance_comment: string | null
+  reference: string | null;
+  summary: string;
+  description: string | null;
+  target_object: string | null;
+  compliance_status: string | null;
+  compliance_comment: string | null;
 }
 
 export interface IRequirement extends IRequirementInput {
-  id: number
-  project: IProject
+  id: number;
+  project: IProject;
 }
 
 export class Requirement implements IRequirement {
@@ -27,14 +27,14 @@ export class Requirement implements IRequirement {
   project: Project;
 
   constructor(requirement: IRequirement) {
-    this.id = requirement.id
-    this.reference = requirement.reference
-    this.summary = requirement.summary
-    this.description = requirement.description
-    this.target_object = requirement.target_object
-    this.compliance_status = requirement.compliance_status
-    this.compliance_comment = requirement.compliance_comment
-    this.project = new Project(requirement.project)
+    this.id = requirement.id;
+    this.reference = requirement.reference;
+    this.summary = requirement.summary;
+    this.description = requirement.description;
+    this.target_object = requirement.target_object;
+    this.compliance_status = requirement.compliance_status;
+    this.compliance_comment = requirement.compliance_comment;
+    this.project = new Project(requirement.project);
   }
 
   toRequirementInput(): IRequirementInput {
@@ -45,56 +45,64 @@ export class Requirement implements IRequirement {
       target_object: this.target_object,
       compliance_status: this.compliance_status,
       compliance_comment: this.compliance_comment,
-    }
+    };
   }
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RequirementService {
-
   constructor(
     protected _crud: CRUDService<IRequirementInput, IRequirement>,
-    protected _projects: ProjectService) {}
+    protected _projects: ProjectService
+  ) {}
 
   getRequirementsUrl(projectId: number): string {
-    return `${this._projects.getProjectUrl(projectId)}/requirements`
+    return `${this._projects.getProjectUrl(projectId)}/requirements`;
   }
 
   getRequirementUrl(requirementId: number): string {
-    return `requirements/${requirementId}`
+    return `requirements/${requirementId}`;
   }
 
   async listRequirements(projectId: number): Promise<Requirement[]> {
     const requirements = await this._crud.list(
-      this.getRequirementsUrl(projectId))
-    return requirements.map(requirement => new Requirement(requirement))
+      this.getRequirementsUrl(projectId)
+    );
+    return requirements.map((requirement) => new Requirement(requirement));
   }
 
   async createRequirement(
-      projectId: number, 
-      requirementInput: IRequirementInput): Promise<Requirement> {
+    projectId: number,
+    requirementInput: IRequirementInput
+  ): Promise<Requirement> {
     const requirement = await this._crud.create(
-      this.getRequirementsUrl(projectId), requirementInput)
-    return new Requirement(requirement)
+      this.getRequirementsUrl(projectId),
+      requirementInput
+    );
+    return new Requirement(requirement);
   }
 
   async getRequirement(requirementId: number): Promise<Requirement> {
     const requirement = await this._crud.read(
-      this.getRequirementUrl(requirementId))
-    return new Requirement(requirement)
+      this.getRequirementUrl(requirementId)
+    );
+    return new Requirement(requirement);
   }
-  
+
   async updateRequirement(
-      requirementId: number, 
-      requirementInput: IRequirementInput): Promise<Requirement> {
+    requirementId: number,
+    requirementInput: IRequirementInput
+  ): Promise<Requirement> {
     const requirement = await this._crud.update(
-      this.getRequirementUrl(requirementId), requirementInput)
-    return new Requirement(requirement)
+      this.getRequirementUrl(requirementId),
+      requirementInput
+    );
+    return new Requirement(requirement);
   }
 
   async deleteRequirement(requirementId: number): Promise<null> {
-    return this._crud.delete(this.getRequirementUrl(requirementId))
+    return this._crud.delete(this.getRequirementUrl(requirementId));
   }
 }
