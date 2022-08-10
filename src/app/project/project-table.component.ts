d0801319c3308f893f71d79bec15b563769c3bba
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-10 19:05:23 +0200

    format project table

--- src/app/project/project-table.component.ts
+++ src/app/project/project-table.component.ts
@@ -6,56 +6,60 @@ import { ProjectDialogComponent } from './project-dialog.component';
 @Component({
   selector: 'mvtool-project-table',
   templateUrl: './project-table.component.html',
-  styles: [
-    '.data-row:hover { cursor: pointer; background-color: #f5f5f5; }',
-  ]
+  styles: ['.data-row:hover { cursor: pointer; background-color: #f5f5f5; }'],
 })
 export class ProjectTableComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'description', 'jira_project_id', 'options'];
-  data: Project[] = []
-  dataLoaded: boolean = false
+  displayedColumns: string[] = [
+    'name',
+    'description',
+    'jira_project_id',
+    'options',
+  ];
+  data: Project[] = [];
+  dataLoaded: boolean = false;
   @Output() projectClicked = new EventEmitter<Project>();
 
   constructor(
-    protected _projectService: ProjectService, 
-    protected _dialog: MatDialog) {}
+    protected _projectService: ProjectService,
+    protected _dialog: MatDialog
+  ) {}
 
   async ngOnInit(): Promise<void> {
-    await this.onReloadProjects()
-    this.dataLoaded = true
+    await this.onReloadProjects();
+    this.dataLoaded = true;
   }
 
   onCreateProject() {
     let dialogRef = this._dialog.open(ProjectDialogComponent, {
-      width: '500px'
-    })
-    dialogRef.afterClosed().subscribe(async projectInput => {
+      width: '500px',
+    });
+    dialogRef.afterClosed().subscribe(async (projectInput) => {
       if (projectInput) {
-        await this._projectService.createProject(projectInput)
-        this.onReloadProjects()
+        await this._projectService.createProject(projectInput);
+        this.onReloadProjects();
       }
-    })
+    });
   }
 
   onEditProject(project: Project) {
     let dialogRef = this._dialog.open(ProjectDialogComponent, {
       width: '500px',
       data: project,
-    })
-    dialogRef.afterClosed().subscribe(async projectInput => {
+    });
+    dialogRef.afterClosed().subscribe(async (projectInput) => {
       if (projectInput) {
-        await this._projectService.updateProject(project.id, projectInput)
-        this.onReloadProjects()
+        await this._projectService.updateProject(project.id, projectInput);
+        this.onReloadProjects();
       }
-    })
+    });
   }
 
   async onDeleteProject(project: Project) {
-    await this._projectService.deleteProject(project.id)
-    this.onReloadProjects()
+    await this._projectService.deleteProject(project.id);
+    this.onReloadProjects();
   }
 
   async onReloadProjects() {
-    this.data = await this._projectService.listProjects()
+    this.data = await this._projectService.listProjects();
   }
 }
