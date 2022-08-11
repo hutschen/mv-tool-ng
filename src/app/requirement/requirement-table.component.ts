fae686f81fdf1b57d3c4f7b8009547376c28aef9
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-11 18:18:01 +0200

    optimize display columns of requirements

--- src/app/requirement/requirement-table.component.ts
+++ src/app/requirement/requirement-table.component.ts
@@ -19,16 +19,6 @@ import {
   styles: [],
 })
 export class RequirementTableComponent implements OnInit {
-  _displayedColumns: string[] = [
-    'reference',
-    'summary',
-    'description',
-    'target_object',
-    'compliance_status',
-    'compliance_comment',
-    'completion',
-    'options',
-  ];
   data: Requirement[] = [];
   dataLoaded: boolean = false;
   @Input() project: Project | null = null;
@@ -53,34 +43,17 @@ export class RequirementTableComponent implements OnInit {
       ['target_object', false],
       ['compliance_status', false],
       ['compliance_comment', false],
-      ['completion', false],
+      ['completion', true],
       ['options', true],
     ]);
 
     for (let requirement of this.data) {
-      if (requirement.reference) {
-        displayFlags.set('reference', true);
-      }
-      if (requirement.summary) {
-        displayFlags.set('summary', true);
-      }
-      if (requirement.description) {
-        displayFlags.set('description', true);
-      }
-      if (requirement.target_object) {
-        displayFlags.set('target_object', true);
-      }
-      if (requirement.compliance_status) {
-        displayFlags.set('compliance_status', true);
-      }
-      if (requirement.compliance_comment) {
-        displayFlags.set('compliance_comment', true);
-      }
-      if (requirement.completion) {
-        displayFlags.set('completion', true);
+      for (let [key, value] of Object.entries(requirement)) {
+        if (value && displayFlags.has(key)) {
+          displayFlags.set(key, true);
+        }
       }
     }
-    // return display columns. A column should be displayed if its flag is true
     let displayedColumns: string[] = [];
     for (let columnName of displayFlags.keys()) {
       if (displayFlags.get(columnName)) {
