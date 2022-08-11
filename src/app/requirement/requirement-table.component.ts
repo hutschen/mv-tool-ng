0efe84ccc3be3b0baa069553c1166ab63cbe433f
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-11 17:44:09 +0200

    display columns of requirements

--- src/app/requirement/requirement-table.component.ts
+++ src/app/requirement/requirement-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { stringToKeyValue } from '@angular/flex-layout/extended/style/style-transforms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from '../shared/services/project.service';
@@ -18,7 +19,7 @@ import {
   styles: [],
 })
 export class RequirementTableComponent implements OnInit {
-  displayedColumns: string[] = [
+  _displayedColumns: string[] = [
     'reference',
     'summary',
     'description',
@@ -42,6 +43,51 @@ export class RequirementTableComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     await this.onReloadRequirements();
     this.dataLoaded = true;
+  }
+
+  get displayedColumns(): string[] {
+    let displayFlags = new Map<string, boolean>([
+      ['reference', false],
+      ['summary', false],
+      ['description', false],
+      ['target_object', false],
+      ['compliance_status', false],
+      ['compliance_comment', false],
+      ['completion', false],
+      ['options', true],
+    ]);
+
+    for (let requirement of this.data) {
+      if (requirement.reference) {
+        displayFlags.set('reference', true);
+      }
+      if (requirement.summary) {
+        displayFlags.set('summary', true);
+      }
+      if (requirement.description) {
+        displayFlags.set('description', true);
+      }
+      if (requirement.target_object) {
+        displayFlags.set('target_object', true);
+      }
+      if (requirement.compliance_status) {
+        displayFlags.set('compliance_status', true);
+      }
+      if (requirement.compliance_comment) {
+        displayFlags.set('compliance_comment', true);
+      }
+      if (requirement.completion) {
+        displayFlags.set('completion', true);
+      }
+    }
+    // return display columns. A column should be displayed if its flag is true
+    let displayedColumns: string[] = [];
+    for (let columnName of displayFlags.keys()) {
+      if (displayFlags.get(columnName)) {
+        displayedColumns.push(columnName);
+      }
+    }
+    return displayedColumns;
   }
 
   onCreateRequirement(): void {
