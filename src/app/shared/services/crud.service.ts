802a30c131dc0f4beee5bca111f3cb4888279653
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-12 18:58:56 +0200

    reformat crud service

--- src/app/shared/services/crud.service.ts
+++ src/app/shared/services/crud.service.ts
@@ -1,58 +1,72 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { firstValueFrom } from "rxjs";
-import { environment } from "src/environments/environment";
-import { AuthService } from "./auth.service";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CRUDService<InputType, OutputType> {  
+export class CRUDService<InputType, OutputType> {
   constructor(
-        protected _httpClient: HttpClient, protected _auth: AuthService) {}
+    protected _httpClient: HttpClient,
+    protected _auth: AuthService
+  ) {}
 
   toAbsoluteUrl(relativeUrl: string): string {
-    return `${environment.baseUrl}/${relativeUrl}`
+    return `${environment.baseUrl}/${relativeUrl}`;
   }
 
   protected get _httpOptions(): object {
-    const credentials = this._auth.credentials
-    const credentials_str = `${credentials.username}:${credentials.password}`
+    const credentials = this._auth.credentials;
+    const credentials_str = `${credentials.username}:${credentials.password}`;
     return {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + btoa(credentials_str)
-      })
-    }
+        Authorization: 'Basic ' + btoa(credentials_str),
+      }),
+    };
   }
-  
+
   async list(relativeUrl: string): Promise<OutputType[]> {
     const request$ = this._httpClient.get<OutputType[]>(
-      this.toAbsoluteUrl(relativeUrl), this._httpOptions)
-    return firstValueFrom(request$)
+      this.toAbsoluteUrl(relativeUrl),
+      this._httpOptions
+    );
+    return firstValueFrom(request$);
   }
-  
+
   async create(relativeUrl: string, itemInput: InputType): Promise<OutputType> {
     const request$ = this._httpClient.post<OutputType>(
-      this.toAbsoluteUrl(relativeUrl), itemInput, this._httpOptions)
-    return firstValueFrom(request$)
+      this.toAbsoluteUrl(relativeUrl),
+      itemInput,
+      this._httpOptions
+    );
+    return firstValueFrom(request$);
   }
-  
-  async read(relativeUrl: string) : Promise<OutputType> {
+
+  async read(relativeUrl: string): Promise<OutputType> {
     const request$ = this._httpClient.get<OutputType>(
-      this.toAbsoluteUrl(relativeUrl), this._httpOptions)
-    return firstValueFrom(request$)
+      this.toAbsoluteUrl(relativeUrl),
+      this._httpOptions
+    );
+    return firstValueFrom(request$);
   }
-  
-  async update(relativeUrl: string, itemInput: InputType) : Promise<OutputType> {
+
+  async update(relativeUrl: string, itemInput: InputType): Promise<OutputType> {
     const request$ = this._httpClient.put<OutputType>(
-      this.toAbsoluteUrl(relativeUrl), itemInput, this._httpOptions)
-    return firstValueFrom(request$)
+      this.toAbsoluteUrl(relativeUrl),
+      itemInput,
+      this._httpOptions
+    );
+    return firstValueFrom(request$);
   }
-  
+
   async delete(relativeUrl: string): Promise<null> {
     const request$ = this._httpClient.delete<null>(
-      this.toAbsoluteUrl(relativeUrl), this._httpOptions)
-    return firstValueFrom(request$)
+      this.toAbsoluteUrl(relativeUrl),
+      this._httpOptions
+    );
+    return firstValueFrom(request$);
   }
 }
