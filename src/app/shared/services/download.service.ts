8fedf49e7e3e765962c895afac2e24e6638462f8
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-08-13 10:46:22 +0200

    sketch download service

--- src/app/shared/services/download.service.ts
+++ src/app/shared/services/download.service.ts
@@ -1,9 +1,93 @@
+import {
+  HttpClient,
+  HttpEvent,
+  HttpEventType,
+  HttpHeaders,
+  HttpProgressEvent,
+  HttpResponse,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, scan } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
+
+export interface IDownloadState {
+  state: 'pending' | 'in_progress' | 'done';
+  progress: number;
+  content: Blob | null;
+}
+
+function download(
+  source: Observable<HttpEvent<Blob>>
+): Observable<IDownloadState> {
+  return source.pipe(
+    scan(
+      (previousState: IDownloadState, event: HttpEvent<Blob>) => {
+        switch (event.type) {
+          case HttpEventType.DownloadProgress:
+            return {
+              ...previousState,
+              state: 'in_progress',
+              progress: event.total
+                ? Math.round((100 * event.loaded) / event.total)
+                : previousState.progress,
+            };
+          case HttpEventType.Response:
+            return {
+              ...previousState,
+              state: 'done',
+              content: event.body,
+            };
+          default:
+            return previousState;
+        }
+      },
+      { state: 'pending', progress: 0, content: null }
+    )
+  );
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DownloadService {
+  constructor(
+    protected _httpClient: HttpClient,
+    protected _auth: AuthService
+  ) {}
 
-  constructor() { }
+  // FIXME: this is a duplicate of the method in the CRUDService
+  toAbsoluteUrl(relativeUrl: string): string {
+    return `${environment.baseUrl}/${relativeUrl}`;
+  }
+
+  // FIXME: this method is currently unused
+  protected _isHttpResponse<T>(event: HttpEvent<T>): event is HttpResponse<T> {
+    return event.type === HttpEventType.Response;
+  }
+
+  // FIXME: this method is currently unused
+  protected _isHttpProgressEvent(
+    event: HttpEvent<unknown>
+  ): event is HttpProgressEvent {
+    return (
+      event.type === HttpEventType.DownloadProgress ||
+      event.type === HttpEventType.UploadProgress
+    );
+  }
+
+  download(relativeUrl: string): Observable<IDownloadState> {
+    const credentials = this._auth.credentials;
+    const credentials_str = `${credentials.username}:${credentials.password}`;
+
+    const request$ = this._httpClient.get(this.toAbsoluteUrl(relativeUrl), {
+      headers: new HttpHeaders({
+        Authorization: 'Basic ' + btoa(credentials_str),
+      }),
+      responseType: 'blob',
+      observe: 'events',
+      reportProgress: true,
+    });
+    return download(request$);
+  }
 }
