27bf7bcb97993f52f553346481643bfc580d851a
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-09-25 16:14:52 +0200

    generate detail component

---
+++ src/app/shared/components/detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mvtool-detail',
+  template: `
+    <p>
+      detail works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
