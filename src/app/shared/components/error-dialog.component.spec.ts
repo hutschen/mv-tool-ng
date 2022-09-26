c2fe9e2725438769c404984e97df9e87f7e3ca64
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-09-26 18:03:35 +0200

    generate error dialog

---
+++ src/app/shared/components/error-dialog.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ErrorDialogComponent } from './error-dialog.component';
+
+describe('ErrorDialogComponent', () => {
+  let component: ErrorDialogComponent;
+  let fixture: ComponentFixture<ErrorDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ErrorDialogComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ErrorDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  xit('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/error-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mvtool-error-dialog',
+  template: `
+    <p>
+      error-dialog works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ErrorDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
