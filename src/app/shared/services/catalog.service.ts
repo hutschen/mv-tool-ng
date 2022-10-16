513eac166b3cf87a2928f97c58e70e0b6eb74272
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-16 13:11:40 +0200

    catalog service operations

--- src/app/shared/services/catalog.service.ts
+++ src/app/shared/services/catalog.service.ts
@@ -1,4 +1,20 @@
+// Copyright (C) 2022 Helmar Hutschenreuter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import { Injectable } from '@angular/core';
+import { CRUDService } from './crud.service';
 
 export interface ICatalogInput {
   reference: string | null;
@@ -36,5 +52,46 @@ export class Catalog implements ICatalog {
   providedIn: 'root',
 })
 export class CatalogService {
-  constructor() {}
+  constructor(protected _crud: CRUDService<ICatalogInput, ICatalog>) {}
+
+  getCatalogsUrl(): string {
+    return 'catalogs';
+  }
+
+  getCatalogUrl(catalogId: number): string {
+    return `${this.getCatalogsUrl()}/${catalogId}`;
+  }
+
+  async listCatalogs(): Promise<Catalog[]> {
+    const catalogs = await this._crud.list(this.getCatalogsUrl());
+    return catalogs.map((catalog) => new Catalog(catalog));
+  }
+
+  async createCatalog(catalogInput: ICatalogInput): Promise<Catalog> {
+    const catalog = await this._crud.create(
+      this.getCatalogsUrl(),
+      catalogInput
+    );
+    return new Catalog(catalog);
+  }
+
+  async getCatalog(catalogId: number): Promise<Catalog> {
+    const catalog = await this._crud.read(this.getCatalogUrl(catalogId));
+    return new Catalog(catalog);
+  }
+
+  async updateCatalog(
+    catalogId: number,
+    catalogInput: ICatalogInput
+  ): Promise<Catalog> {
+    const catalog = await this._crud.update(
+      this.getCatalogUrl(catalogId),
+      catalogInput
+    );
+    return new Catalog(catalog);
+  }
+
+  async deleteCatalog(catalogId: number): Promise<void> {
+    await this._crud.delete(this.getCatalogUrl(catalogId));
+  }
 }
