f148d9faad56a6da91b33cb727149d3a20d69e16
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-16 12:59:23 +0200

    add catalog interfaces and class

--- src/app/shared/services/catalog.service.ts
+++ src/app/shared/services/catalog.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
 
+export interface ICatalogInput {
+  reference: string | null;
+  title: string;
+  description: string | null;
+}
+
+export interface ICatalog extends ICatalogInput {
+  id: number;
+}
+
+export class Catalog implements ICatalog {
+  id: number;
+  reference: string | null;
+  title: string;
+  description: string | null;
+
+  constructor(catalog: ICatalog) {
+    this.id = catalog.id;
+    this.reference = catalog.reference;
+    this.title = catalog.title;
+    this.description = catalog.description;
+  }
+
+  toCatalogInput(): ICatalogInput {
+    return {
+      reference: this.reference,
+      title: this.title,
+      description: this.description,
+    };
+  }
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CatalogService {
-
-  constructor() { }
+  constructor() {}
 }
