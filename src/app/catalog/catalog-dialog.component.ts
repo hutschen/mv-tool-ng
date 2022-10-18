e97d82c5098630e7a75443fcee61d9674ce493d5
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-18 20:05:35 +0200

    logic for catalog dialog component

--- src/app/catalog/catalog-dialog.component.ts
+++ src/app/catalog/catalog-dialog.component.ts
@@ -13,15 +13,57 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  Catalog,
+  CatalogService,
+  ICatalogInput,
+} from '../shared/services/catalog.service';
 
 @Component({
   selector: 'mvtool-catalog-dialog',
   templateUrl: './catalog-dialog.component.html',
   styles: [],
 })
-export class CatalogDialogComponent implements OnInit {
-  constructor() {}
+export class CatalogDialogComponent {
+  catalogInput: ICatalogInput = {
+    reference: null,
+    title: '',
+    description: null,
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    protected _dialogRef: MatDialogRef<CatalogDialogComponent>,
+    protected _catalogService: CatalogService,
+    @Inject(MAT_DIALOG_DATA) protected _catalog: Catalog | null
+  ) {
+    if (this._catalog) {
+      this.catalogInput = this._catalog.toCatalogInput();
+    }
+  }
+
+  get createMode(): boolean {
+    return !this._catalog;
+  }
+
+  async onSave(form: NgForm): Promise<void> {
+    if (form.valid) {
+      let catalog: Catalog;
+      if (!this._catalog) {
+        catalog = await this._catalogService.createCatalog(this.catalogInput);
+      } else {
+        catalog = await this._catalogService.updateCatalog(
+          this._catalog.id,
+          this.catalogInput
+        );
+      }
+      this._dialogRef.close(catalog);
+    }
+  }
+
+  onCancel(): void {
+    this._dialogRef.close(null);
+  }
 }
