914b0dc6f3ff3e598439646fa16965a47fb1ed70
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-20 16:44:25 +0200

    generate breadcrumb trail module and its components

---
+++ src/app/breadcrumb-trail/breadcrumb-trail.component.spec.ts
@@ -0,0 +1,37 @@
+// Copyright (C) 2022 Helmar Hutschenreuter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BreadcrumbTrailComponent } from './breadcrumb-trail.component';
+
+describe('BreadcrumbTrailComponent', () => {
+  let component: BreadcrumbTrailComponent;
+  let fixture: ComponentFixture<BreadcrumbTrailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BreadcrumbTrailComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BreadcrumbTrailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  xit('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
