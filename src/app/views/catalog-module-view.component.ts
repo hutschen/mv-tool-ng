562ec58e9d00ddb016117d6fc73083d4bf2a142d
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-22 13:42:21 +0200

    logic of catalog module view

--- src/app/views/catalog-module-view.component.ts
+++ src/app/views/catalog-module-view.component.ts
@@ -13,7 +13,11 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CatalogModule } from '../shared/services/catalog-module.service';
+import { Catalog, CatalogService } from '../shared/services/catalog.service';
 
 @Component({
   selector: 'mvtool-catalog-module-view',
@@ -21,7 +25,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class CatalogModuleViewComponent implements OnInit {
-  constructor() {}
+  catalog: Catalog | null = null;
 
-  ngOnInit(): void {}
+  constructor(
+    protected _route: ActivatedRoute,
+    protected _router: Router,
+    protected _catalogService: CatalogService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const catalogId = Number(this._route.snapshot.paramMap.get('catalogId'));
+    try {
+      this.catalog = await this._catalogService.getCatalog(catalogId);
+    } catch (error: any) {
+      if (error instanceof HttpErrorResponse && error.status === 404) {
+        this._router.navigate(['/']);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  onCatalogClicked(catalogModule: CatalogModule): void {
+    this._router.navigate([
+      '/catalog-modules',
+      catalogModule.id,
+      'catalog-requirements',
+    ]);
+  }
 }
