e19dc87238fb5d418dd83e522d3a5e626252cd22
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-22 17:46:33 +0200

    logic of catalog module dialog component

--- src/app/catalog-module/catalog-module-dialog.component.ts
+++ src/app/catalog-module/catalog-module-dialog.component.ts
@@ -13,15 +13,70 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  CatalogModule,
+  CatalogModuleService,
+  ICatalogModuleInput,
+} from '../shared/services/catalog-module.service';
+import { Catalog } from '../shared/services/catalog.service';
+
+export interface ICatalogModuleDialogData {
+  catalog: Catalog;
+  catalogModule: CatalogModule | null;
+}
 
 @Component({
   selector: 'mvtool-catalog-module-dialog',
   templateUrl: './catalog-module-dialog.component.html',
   styles: [],
 })
-export class CatalogModuleDialogComponent implements OnInit {
-  constructor() {}
+export class CatalogModuleDialogComponent {
+  catalog: Catalog;
+  catalogModuleInput: ICatalogModuleInput = {
+    reference: null,
+    title: '',
+    description: null,
+    gs_reference: null,
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    protected _dialogRef: MatDialogRef<CatalogModuleDialogComponent>,
+    protected _catalogModuleService: CatalogModuleService,
+    @Inject(MAT_DIALOG_DATA) protected _dialogData: ICatalogModuleDialogData
+  ) {
+    this.catalog = this._dialogData.catalog;
+    if (this._dialogData.catalogModule) {
+      this.catalogModuleInput =
+        this._dialogData.catalogModule.toCatalogModuleInput();
+    }
+  }
+
+  get createMode(): boolean {
+    return !this._dialogData.catalogModule;
+  }
+
+  async onSave(form: NgForm): Promise<void> {
+    if (form.valid) {
+      let catalogModule: CatalogModule;
+      if (!this._dialogData.catalogModule) {
+        catalogModule = await this._catalogModuleService.createCatalogModule(
+          this.catalog.id,
+          this.catalogModuleInput
+        );
+      } else {
+        catalogModule = await this._catalogModuleService.updateCatalogModule(
+          this._dialogData.catalogModule.id,
+          this.catalogModuleInput
+        );
+      }
+      this._dialogRef.close(catalogModule);
+    }
+  }
+
+  onCancel(): void {
+    this._dialogRef.close(null);
+  }
 }
