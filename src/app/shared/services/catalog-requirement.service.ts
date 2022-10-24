80f255a8af1ba4e094eed6410f9da7a15ae00b9a
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-24 18:06:49 +0200

    define interfaces and class for catalog requirement

--- src/app/shared/services/catalog-requirement.service.ts
+++ src/app/shared/services/catalog-requirement.service.ts
@@ -14,6 +14,63 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Injectable } from '@angular/core';
+import { CatalogModule, ICatalogModule } from './catalog-module.service';
+import { IRequirementInput } from './requirement.service';
+
+export interface ICatalogRequirementInput extends IRequirementInput {}
+
+export interface ICatalogRequirement extends ICatalogRequirementInput {
+  id: number;
+  catalog_module: ICatalogModule;
+
+  // Special attributes for IT Grundschutz Kompendium
+  gs_anforderung_reference: string | null;
+  gs_absicherung: string | null;
+  gs_verantwortliche: string | null;
+}
+
+export class CatalogRequirement implements ICatalogRequirement {
+  id: number;
+  reference: string | null;
+  summary: string;
+  description: string | null;
+  target_object: string | null;
+  compliance_status: string | null;
+  compliance_comment: string | null;
+  catalog_module: CatalogModule;
+
+  // Special attributes for IT Grundschutz Kompendium
+  gs_anforderung_reference: string | null;
+  gs_absicherung: string | null;
+  gs_verantwortliche: string | null;
+
+  constructor(catalogRequirement: ICatalogRequirement) {
+    this.id = catalogRequirement.id;
+    this.reference = catalogRequirement.reference;
+    this.summary = catalogRequirement.summary;
+    this.description = catalogRequirement.description;
+    this.target_object = catalogRequirement.target_object;
+    this.compliance_status = catalogRequirement.compliance_status;
+    this.compliance_comment = catalogRequirement.compliance_comment;
+    this.catalog_module = new CatalogModule(catalogRequirement.catalog_module);
+
+    // Special attributes for IT Grundschutz Kompendium
+    this.gs_anforderung_reference = catalogRequirement.gs_anforderung_reference;
+    this.gs_absicherung = catalogRequirement.gs_absicherung;
+    this.gs_verantwortliche = catalogRequirement.gs_verantwortliche;
+  }
+
+  toCatalogRequirementInput(): ICatalogRequirementInput {
+    return {
+      reference: this.reference,
+      summary: this.summary,
+      description: this.description,
+      target_object: this.target_object,
+      compliance_status: this.compliance_status,
+      compliance_comment: this.compliance_comment,
+    };
+  }
+}
 
 @Injectable({
   providedIn: 'root',
