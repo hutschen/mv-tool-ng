34687a4d971979a23b53f8b48215004dc6272019
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-10-26 20:13:38 +0200

    logic of catalog requirement dialog component

--- src/app/catalog-requirement/catalog-requirement-dialog.component.ts
+++ src/app/catalog-requirement/catalog-requirement-dialog.component.ts
@@ -13,15 +13,75 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CatalogModule } from '../shared/services/catalog-module.service';
+import {
+  CatalogRequirement,
+  CatalogRequirementService,
+  ICatalogRequirementInput,
+} from '../shared/services/catalog-requirement.service';
+
+export interface ICatalogRequirementDialogData {
+  catalogModule: CatalogModule;
+  catalogRequirement?: CatalogRequirement;
+}
 
 @Component({
   selector: 'mvtool-catalog-requirement-dialog',
   templateUrl: './catalog-requirement-dialog.component.html',
   styles: [],
 })
-export class CatalogRequirementDialogComponent implements OnInit {
-  constructor() {}
+export class CatalogRequirementDialogComponent {
+  catalogModule: CatalogModule;
+  catalogRequirementInput: ICatalogRequirementInput = {
+    reference: null,
+    summary: '',
+    description: null,
+    target_object: null,
+    compliance_status: null,
+    compliance_comment: null,
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    protected _dialogRef: MatDialogRef<CatalogRequirementDialogComponent>,
+    protected _catalogRequirementService: CatalogRequirementService,
+    @Inject(MAT_DIALOG_DATA)
+    protected _dialogData: ICatalogRequirementDialogData
+  ) {
+    this.catalogModule = this._dialogData.catalogModule;
+    if (this._dialogData.catalogRequirement) {
+      this.catalogRequirementInput =
+        this._dialogData.catalogRequirement.toCatalogRequirementInput();
+    }
+  }
+
+  get createMode(): boolean {
+    return !this._dialogData.catalogRequirement;
+  }
+
+  async onSave(form: NgForm): Promise<void> {
+    if (form.valid) {
+      let catalogRequirement: CatalogRequirement;
+      if (!this._dialogData.catalogRequirement) {
+        catalogRequirement =
+          await this._catalogRequirementService.createCatalogRequirement(
+            this.catalogModule.id,
+            this.catalogRequirementInput
+          );
+      } else {
+        catalogRequirement =
+          await this._catalogRequirementService.updateCatalogRequirement(
+            this._dialogData.catalogRequirement.id,
+            this.catalogRequirementInput
+          );
+      }
+      this._dialogRef.close(catalogRequirement);
+    }
+  }
+
+  onCancel(): void {
+    this._dialogRef.close();
+  }
 }
