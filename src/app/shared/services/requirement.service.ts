2d24eddc195560739b73fcfa7ab07ceb8c440d83
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-12-23 20:12:19 +0100

    remove unused import in requirement service

--- src/app/shared/services/requirement.service.ts
+++ src/app/shared/services/requirement.service.ts
@@ -14,7 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Injectable } from '@angular/core';
-import { firstValueFrom, map, Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import {
   CatalogRequirement,
   ICatalogRequirement,
