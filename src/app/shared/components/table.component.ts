a9c10d499747f2e79f443fd921ac7fdb0a5c2d23
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-01-24 15:10:51 +0100

    logic for generic table component

--- src/app/shared/components/table.component.ts
+++ src/app/shared/components/table.component.ts
@@ -13,15 +13,66 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChildren,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  ViewChild,
+} from '@angular/core';
+import { MatColumnDef, MatTable } from '@angular/material/table';
+import { ITableRow, TableColumn, TableColumns } from '../table-columns';
 
 @Component({
   selector: 'mvtool-table',
   templateUrl: './table.component.html',
   styles: [],
 })
-export class TableComponent implements OnInit {
+export class TableComponent<T extends object> implements AfterContentInit {
+  @Input() columns: TableColumn<T>[] = [];
+  @Input() data: T[] = [];
+  @Input() noContentLabel = 'Nothing to display.';
+  @Input() loadingLabel = 'Loading...';
+  @Input() createLabel = 'Create One';
+  @Output() clickRow = new EventEmitter<T>();
+  @Output() create = new EventEmitter<void>();
+
+  @ViewChild(MatTable, { static: true }) matTable!: MatTable<T>;
+  @ContentChildren(MatColumnDef) matColumnDefs!: QueryList<MatColumnDef>;
+
+  columnsToAutoCreate: TableColumn<T>[] = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngAfterContentInit(): void {
+    const idsOfColumnsDefined: string[] = this.matColumnDefs.map(
+      (matColumnDef) => {
+        this.matTable.addColumnDef(matColumnDef);
+        return matColumnDef.name;
+      }
+    );
+    this.columnsToAutoCreate = this.columns.filter(
+      (column) => !idsOfColumnsDefined.includes(column.id)
+    );
+  }
+
+  get idsOfColumnsToShow(): string[] {
+    return new TableColumns(this.columns)
+      .columnsToShow(this.data)
+      .map((column) => column.id);
+  }
+
+  get rows(): ITableRow<T>[] {
+    return this.data.map((data) => {
+      return {
+        data: data,
+        ...Object.fromEntries(
+          this.columns.map((column) => [column.id, column.toStr(data)])
+        ),
+      } as ITableRow<T>;
+    });
+  }
 }
