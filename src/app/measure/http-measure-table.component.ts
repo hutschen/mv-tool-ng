9adcb54f405998d4b400eb9eb3a907b7f802a0c8
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-01-25 14:55:21 +0100

    add toolbar and options menu to http measure table

--- src/app/measure/http-measure-table.component.ts
+++ src/app/measure/http-measure-table.component.ts
@@ -13,10 +13,20 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { map, merge, startWith, switchMap } from 'rxjs';
+import { firstValueFrom, map, merge, startWith, switchMap } from 'rxjs';
+import { ComplianceDialogService } from '../shared/components/compliance-dialog.component';
+import { ConfirmDialogService } from '../shared/components/confirm-dialog.component';
+import { DownloadDialogService } from '../shared/components/download-dialog.component';
+import { UploadDialogService } from '../shared/components/upload-dialog.component';
 import {
   DataColumn,
   DataField,
@@ -26,13 +36,15 @@ import {
 import { Measure, MeasureService } from '../shared/services/measure.service';
 import { Project } from '../shared/services/project.service';
 import { Requirement } from '../shared/services/requirement.service';
-import { ITableColumn, TableColumn } from '../shared/table-columns';
+import { CompletionDialogService } from './completion-dialog.component';
+import { MeasureDialogService } from './measure-dialog.component';
 import {
   DocumentField,
   JiraIssueField,
   StatusField,
   VerifiedField,
 } from './measure-fields';
+import { VerificationDialogService } from './verification-dialog.component';
 
 @Component({
   selector: 'mvtool-http-measure-table',
@@ -77,17 +89,27 @@ export class HttpMeasureTableComponent implements AfterViewInit {
   resultsLength = 0;
   isLoadingData = true;
   isRateLimitReached = false;
+  reload = new EventEmitter<void>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(protected _measureService: MeasureService) {}
+  constructor(
+    protected _measureService: MeasureService,
+    protected _measureDialogService: MeasureDialogService,
+    protected _complianceDialogService: ComplianceDialogService,
+    protected _completionDialogService: CompletionDialogService,
+    protected _verificationDialogService: VerificationDialogService,
+    protected _downloadDialogService: DownloadDialogService,
+    protected _uploadDialogService: UploadDialogService,
+    protected _confirmDialogService: ConfirmDialogService
+  ) {}
 
   ngAfterViewInit(): void {
     // When the user changes the sort order, reset to the first page
     this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
 
-    merge(this.sort.sortChange, this.paginator.page)
+    merge(this.sort.sortChange, this.paginator.page, this.reload)
       .pipe(
         startWith({}),
         switchMap(() => {
@@ -110,5 +132,102 @@ export class HttpMeasureTableComponent implements AfterViewInit {
         })
       )
       .subscribe((data) => (this.dataFrame.data = data));
+  }
+
+  protected async _createOrEditMeasure(measure?: Measure): Promise<void> {
+    if (this.requirement) {
+      const dialogRef = this._measureDialogService.openMeasureDialog(
+        this.requirement,
+        measure
+      );
+      const resultingMeasure = await firstValueFrom(dialogRef.afterClosed());
+      if (resultingMeasure) {
+        this.onReloadMeasures();
+      }
+    } else {
+      throw new Error('Requirement is undefined');
+    }
+  }
+
+  async onCreateMeasure(): Promise<void> {
+    await this._createOrEditMeasure();
+  }
+
+  async onEditMeasure(measure: Measure): Promise<void> {
+    await this._createOrEditMeasure(measure);
+  }
+
+  async onEditCompliance(measure: Measure): Promise<void> {
+    const dialogRef =
+      this._complianceDialogService.openComplianceDialog(measure);
+    const updatedMeasure = await firstValueFrom(dialogRef.afterClosed());
+    if (updatedMeasure) {
+      this.onReloadMeasures();
+    }
+  }
+
+  async onEditCompletion(measure: Measure): Promise<void> {
+    const dialogRef =
+      this._completionDialogService.openCompletionDialog(measure);
+    const updatedMeasure = await firstValueFrom(dialogRef.afterClosed());
+    if (updatedMeasure) {
+      this.onReloadMeasures();
+    }
+  }
+
+  async onEditVerification(measure: Measure): Promise<void> {
+    const dialogRef =
+      this._verificationDialogService.openVerificationDialog(measure);
+    const updatedMeasure = await firstValueFrom(dialogRef.afterClosed());
+    if (updatedMeasure) {
+      this.onReloadMeasures();
+    }
+  }
+
+  async onDeleteMeasure(measure: Measure): Promise<void> {
+    const confirmDialogRef = this._confirmDialogService.openConfirmDialog(
+      'Delete Measure',
+      `Do you really want to delete measure "${measure.summary}"?`
+    );
+    const confirmed = await firstValueFrom(confirmDialogRef.afterClosed());
+    if (confirmed) {
+      await firstValueFrom(this._measureService.deleteMeasure(measure.id));
+      this.onReloadMeasures();
+    }
+  }
+
+  async onExportMeasures(): Promise<void> {
+    if (this.requirement) {
+      const dialogRef = this._downloadDialogService.openDownloadDialog(
+        this._measureService.downloadMeasureExcel(this.requirement.id),
+        'measure.xlsx'
+      );
+      await firstValueFrom(dialogRef.afterClosed());
+    } else {
+      throw new Error('Requirement is undefined');
+    }
+  }
+
+  async onImportMeasures(): Promise<void> {
+    const dialogRef = this._uploadDialogService.openUploadDialog(
+      (file: File) => {
+        if (this.requirement) {
+          return this._measureService.uploadMeasureExcel(
+            this.requirement.id,
+            file
+          );
+        } else {
+          throw new Error('Requirement is undefined');
+        }
+      }
+    );
+    const uploadState = await firstValueFrom(dialogRef.afterClosed());
+    if (uploadState && uploadState.state == 'done') {
+      this.onReloadMeasures();
+    }
+  }
+
+  onReloadMeasures(): void {
+    this.reload.emit();
   }
 }
