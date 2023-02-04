41e1d61f168a98c5c261fbb9bb950737ee0f0308
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-02-04 10:25:02 +0100

    implement table options component

--- src/app/shared/components/table-options.component.ts
+++ src/app/shared/components/table-options.component.ts
@@ -13,15 +13,67 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'mvtool-table-options',
-  template: ` <p>table-options works!</p> `,
+  template: `
+    <div *ngIf="enabled" class="fx-row fx-end-center">
+      <button
+        mat-icon-button
+        [matMenuTriggerFor]="menu"
+        (click)="$event.stopPropagation()"
+        aria-label="Show table options"
+      >
+        <mat-icon>more_vert</mat-icon>
+      </button>
+      <mat-menu #menu="matMenu">
+        <button
+          mat-menu-item
+          *ngIf="showHideColumns.observed"
+          (click)="showHideColumns.emit($event)"
+        >
+          <mat-icon>visibility_off</mat-icon>
+          Show/hide columns
+        </button>
+        <button
+          mat-menu-item
+          *ngIf="clearFilters.observed"
+          (click)="clearFilters.emit($event)"
+        >
+          <mat-icon>filter_alt_off</mat-icon>
+          Clear all filters
+        </button>
+        <button
+          mat-menu-item
+          *ngIf="clearSort.observed"
+          (click)="clearSort.emit($event)"
+        >
+          <mat-icon>sort_off</mat-icon>
+          Clear sort
+        </button>
+
+        <ng-content></ng-content>
+      </mat-menu>
+    </div>
+  `,
+  styleUrls: ['../styles/flex.scss'],
   styles: [],
 })
 export class TableOptionsComponent implements OnInit {
+  @Output() showHideColumns = new EventEmitter<Event>();
+  @Output() clearFilters = new EventEmitter<Event>();
+  @Output() clearSort = new EventEmitter<Event>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  get enabled(): boolean {
+    return Boolean(
+      this.showHideColumns.observed ||
+        this.clearFilters.observed ||
+        this.clearSort.observed
+    );
+  }
 }
