31fb0ffdbd7b796ebd0eed7fee6295b8dbb11deb
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-02-06 17:08:31 +0100

    function to convert query params coming from router

--- src/app/shared/services/query-params.service.ts
+++ src/app/shared/services/query-params.service.ts
@@ -14,6 +14,12 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { mapValues } from 'radash';
+
+interface IRawQueryParams {
+  [param: string]: string | ReadonlyArray<string>;
+}
 
 export interface IQueryParams {
   [param: string]:
@@ -23,9 +29,48 @@ export interface IQueryParams {
     | ReadonlyArray<string | number | boolean>;
 }
 
+function isBool(value: string): boolean {
+  return value.toLowerCase() === 'true' || value.toLowerCase() === 'false';
+}
+
+function convertToBool(value: string): boolean {
+  if (value.toLowerCase() === 'true') return true;
+  else return false;
+}
+
+function isInt(value: string): boolean {
+  return /^\d+$/.test(value);
+}
+
+function convertToInt(value: string): number {
+  return parseInt(value, 10);
+}
+
+function convertToQueryParams(rawQueryParams: IRawQueryParams): IQueryParams {
+  return mapValues(rawQueryParams, (value) => {
+    if (Array.isArray(value)) {
+      if (value.every((v) => isBool(v))) {
+        return value.map((v) => convertToBool(v));
+      } else if (value.every((v) => isInt(v))) {
+        return value.map((v) => convertToInt(v));
+      } else {
+        return value;
+      }
+    } else {
+      if (isBool(value as string)) {
+        return convertToBool(value as string);
+      } else if (isInt(value as string)) {
+        return convertToInt(value as string);
+      } else {
+        return value;
+      }
+    }
+  });
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class QueryParamsService {
-  constructor() {}
+  constructor(protected _router: Router) {}
 }
