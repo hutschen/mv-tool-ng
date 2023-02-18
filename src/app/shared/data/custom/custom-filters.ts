1b9705d0712248451fd4d71df926c264904116f2
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-02-18 10:36:04 +0100

    add target object filter

--- src/app/shared/data/custom/custom-filters.ts
+++ src/app/shared/data/custom/custom-filters.ts
@@ -16,7 +16,9 @@
 import { map, Observable } from 'rxjs';
 import { CatalogModuleService } from '../../services/catalog-module.service';
 import { Catalog, CatalogService } from '../../services/catalog.service';
+import { Project } from '../../services/project.service';
 import { IQueryParams } from '../../services/query-params.service';
+import { TargetObjectService } from '../../services/target-object.service';
 import { FilterByValues, IFilterOption } from '../filter';
 
 export class CatalogFilter extends FilterByValues {
@@ -117,6 +119,57 @@ export class CatalogModuleFilter extends FilterByValues {
       catalog_module_ids: values,
     };
     if (this._catalog) queryParams['catalog_ids'] = this._catalog.id;
+    return this.__loadOptions(queryParams);
+  }
+}
+
+export class TargetObjectFilter extends FilterByValues {
+  constructor(
+    protected _targetObjectService: TargetObjectService,
+    protected _project: Project,
+    initQueryParams: IQueryParams = {}
+  ) {
+    super('target_objects', undefined, initQueryParams);
+    this.loadOptions();
+  }
+
+  private __loadOptions(
+    queryParams: IQueryParams
+  ): Observable<IFilterOption[]> {
+    // Request target objects and convert them to filter options
+    return this._targetObjectService.getTargetObjects(queryParams).pipe(
+      map((targetObjects) => {
+        if (!Array.isArray(targetObjects)) targetObjects = targetObjects.items;
+        return targetObjects.map((targetObject) => ({
+          value: targetObject,
+          label: targetObject,
+        }));
+      })
+    );
+  }
+
+  override getOptions(
+    searchStr: string | null = null,
+    limit: number = -1
+  ): Observable<IFilterOption[]> {
+    // Build query params to request target objects
+    const queryParams: IQueryParams = { project_ids: this._project.id };
+    if (searchStr) queryParams['local_search'] = searchStr;
+    if (limit) {
+      queryParams['page'] = 1;
+      queryParams['page_size'] = limit;
+    }
+
+    return this.__loadOptions(queryParams);
+  }
+
+  override getOptionsByValues(
+    values: (string | number)[]
+  ): Observable<IFilterOption[]> {
+    const queryParams: IQueryParams = {
+      target_objects: values,
+      project_ids: this._project.id,
+    };
     return this.__loadOptions(queryParams);
   }
 }
