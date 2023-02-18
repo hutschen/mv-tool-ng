6d012c3e9a65851cb85887572ca1f5217c6edc2a
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-02-18 15:03:18 +0100

    correct name of verified filter in measure data frame

--- src/app/shared/data/measure/measure-frame.ts
+++ src/app/shared/data/measure/measure-frame.ts
@@ -140,7 +140,7 @@ export class MeasureDataFrame extends DataFrame<Measure> {
         'Verified',
         undefined,
         undefined,
-        new FilterForExistence('has_verified', initQueryParams)
+        new FilterForExistence('verified', initQueryParams)
       ),
       initQueryParams
     );
