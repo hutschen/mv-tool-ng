4911abb606c661f6a789773724162637baae4f88
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-02-25 16:41:23 +0100

    generate project measure view component

--- src/app/project-measure/project-measure-view.component.ts
+++ src/app/project-measure/project-measure-view.component.ts
@@ -13,14 +13,15 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProjectMeasureViewComponent } from './project-measure-view.component';
+import { Component, OnInit } from '@angular/core';
 
-@NgModule({
-  declarations: [
-    ProjectMeasureViewComponent
-  ],
-  imports: [CommonModule],
+@Component({
+  selector: 'mvtool-project-measure-view',
+  template: ` <p>project-measure-view works!</p> `,
+  styles: [],
 })
-export class ProjectMeasureModule {}
+export class ProjectMeasureViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
