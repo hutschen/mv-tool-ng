ff8da63a9f66497cd718eb3eea458276a8c83531
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-02-26 20:58:07 +0100

    remove unused import from requirement data frame

--- src/app/shared/data/requirement/requirement-frame.ts
+++ src/app/shared/data/requirement/requirement-frame.ts
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { CatalogModuleService } from '../../services/catalog-module.service';
 import { CatalogService } from '../../services/catalog.service';
 import { MilestoneService } from '../../services/milestone.service';
