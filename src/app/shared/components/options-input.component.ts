7861ff97c009dfe49ba8982365a436841d5e0b93
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-04-20 19:53:12 +0200

    generate options input component

---
+++ src/app/shared/components/options-input.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mvtool-options-input',
+  template: ` <p>options-input works!</p> `,
+  styles: [],
+})
+export class OptionsInputComponent {}
