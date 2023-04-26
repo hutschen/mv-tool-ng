9d01155bf2da7edb5bfcebdb9ba7cd21212260dd
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-04-26 20:56:37 +0200

    refactor catalog modules and catalog module references filters to use options class

--- src/app/shared/data/catalog-module/catalog-module-filters.ts
+++ src/app/shared/data/catalog-module/catalog-module-filters.ts
@@ -13,11 +13,14 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { map, Observable } from 'rxjs';
 import { CatalogModuleService } from '../../services/catalog-module.service';
 import { Catalog } from '../../services/catalog.service';
 import { IQueryParams } from '../../services/query-params.service';
-import { FilterByValues, IFilterOption } from '../filter';
+import { FilterByValues } from '../filter';
+import {
+  CatalogModuleOptions,
+  CatalogModuleReferenceOptions,
+} from './catalog-module-options';
 
 export class CatalogModuleReferencesFilter extends FilterByValues {
   constructor(
@@ -25,51 +28,12 @@ export class CatalogModuleReferencesFilter extends FilterByValues {
     protected _catalog: Catalog,
     initQueryParams: IQueryParams = {}
   ) {
-    super('references', undefined, initQueryParams);
-    this.loadOptions();
-  }
-
-  private __loadOptions(
-    queryParams: IQueryParams
-  ): Observable<IFilterOption[]> {
-    // Request catalog module references and convert them to filter options
-    return this._catalogModuleService
-      .getCatalogModuleReferences(queryParams)
-      .pipe(
-        map((references) => {
-          if (!Array.isArray(references)) references = references.items;
-          return references.map((reference) => ({
-            value: reference,
-            label: reference,
-          }));
-        })
-      );
-  }
-
-  override getOptions(
-    searchStr: string | null = null,
-    limit: number = -1
-  ): Observable<IFilterOption[]> {
-    // Build query params to request catalog module references
-    const queryParams: IQueryParams = {
-      catalog_ids: this._catalog.id,
-    };
-    if (searchStr) queryParams['local_search'] = searchStr;
-    if (limit) {
-      queryParams['page'] = 1;
-      queryParams['page_size'] = limit;
-    }
-
-    return this.__loadOptions(queryParams);
-  }
-
-  override getOptionsByValues(values: string[]): Observable<IFilterOption[]> {
-    // Build query params to request catalog module references
-    const queryParams: IQueryParams = {
-      catalog_ids: this._catalog.id,
-      references: values,
-    };
-    return this.__loadOptions(queryParams);
+    super(
+      'references',
+      new CatalogModuleReferenceOptions(_catalogModuleService, _catalog, true),
+      initQueryParams,
+      'string'
+    );
   }
 }
 
@@ -79,51 +43,11 @@ export class CatalogModulesFilter extends FilterByValues {
     protected _catalog?: Catalog,
     initQueryParams: IQueryParams = {}
   ) {
-    super('catalog_module_ids', undefined, initQueryParams);
-    this.loadOptions();
-  }
-
-  private __loadOptions(
-    queryParams: IQueryParams
-  ): Observable<IFilterOption[]> {
-    // Request catalog modules representations and convert them to filter options
-    return this._catalogModuleService
-      .getCatalogModuleRepresentations(queryParams)
-      .pipe(
-        map((catalogModuleReprs) => {
-          if (!Array.isArray(catalogModuleReprs))
-            catalogModuleReprs = catalogModuleReprs.items;
-          return catalogModuleReprs.map((cmr) => ({
-            value: cmr.id,
-            label: (cmr.reference ? cmr.reference + ' ' : '') + cmr.title,
-          }));
-        })
-      );
-  }
-
-  override getOptions(
-    searchStr: string | null = null,
-    limit: number = -1
-  ): Observable<IFilterOption[]> {
-    // Build query params to request catalog modules representations
-    const queryParams = new Object() as IQueryParams;
-    if (this._catalog) queryParams['catalog_ids'] = this._catalog.id;
-    if (searchStr) queryParams['local_search'] = searchStr;
-    if (limit) {
-      queryParams['page'] = 1;
-      queryParams['page_size'] = limit;
-    }
-
-    return this.__loadOptions(queryParams);
-  }
-
-  override getOptionsByValues(
-    values: (string | number)[]
-  ): Observable<IFilterOption[]> {
-    const queryParams: IQueryParams = {
-      ids: values,
-    };
-    if (this._catalog) queryParams['catalog_ids'] = this._catalog.id;
-    return this.__loadOptions(queryParams);
+    super(
+      'catalog_module_ids',
+      new CatalogModuleOptions(_catalogModuleService, _catalog, true),
+      initQueryParams,
+      'number'
+    );
   }
 }
