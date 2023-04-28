d61cd2285979ea93d24270243418dd230cb12c76
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-04-28 21:47:53 +0200

    implement create, edit and delete methods on MeasureInteractionService

--- src/app/shared/services/measure-interaction.service.ts
+++ src/app/shared/services/measure-interaction.service.ts
@@ -14,10 +14,98 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Injectable } from '@angular/core';
+import { Measure, MeasureService } from './measure.service';
+import { MeasureDialogService } from 'src/app/measure/measure-dialog.component';
+import { ComplianceDialogService } from '../components/compliance-dialog.component';
+import { CompletionDialogService } from 'src/app/measure/completion-dialog.component';
+import { VerificationDialogService } from 'src/app/measure/verification-dialog.component';
+import { ConfirmDialogService } from '../components/confirm-dialog.component';
+import { Requirement } from './requirement.service';
+import { Subject, firstValueFrom } from 'rxjs';
+
+export interface InteractionChange<T> {
+  item: T;
+  action: 'create' | 'update' | 'delete';
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class MeasureInteractionService {
-  constructor() {}
+  protected _changesSubject = new Subject<InteractionChange<Measure>>();
+  changes$ = this._changesSubject.asObservable();
+
+  constructor(
+    protected _measureService: MeasureService,
+    protected _measureDialogService: MeasureDialogService,
+    protected _complianceDialogService: ComplianceDialogService,
+    protected _completionDialogService: CompletionDialogService,
+    protected _verificationDialogService: VerificationDialogService,
+    protected _confirmDialogService: ConfirmDialogService
+  ) {}
+
+  protected async _createOrEditMeasure(
+    requirement: Requirement,
+    measure?: Measure
+  ): Promise<void> {
+    const dialogRef = this._measureDialogService.openMeasureDialog(
+      requirement,
+      measure
+    );
+    const resultingMeasure = await firstValueFrom(dialogRef.afterClosed());
+    if (resultingMeasure) {
+      const action = measure ? 'update' : 'create';
+      this._changesSubject.next({ item: resultingMeasure, action: action });
+    }
+  }
+
+  async onCreateMeasure(requirement: Requirement): Promise<void> {
+    await this._createOrEditMeasure(requirement);
+  }
+
+  async onEditMeasure(measure: Measure): Promise<void> {
+    await this._createOrEditMeasure(measure.requirement, measure);
+  }
+
+  async onEditCompliance(measure: Measure): Promise<void> {
+    const dialogRef =
+      this._complianceDialogService.openComplianceDialog(measure);
+    const updatedMeasure = await firstValueFrom(dialogRef.afterClosed());
+    if (updatedMeasure) {
+      this._changesSubject.next({
+        item: updatedMeasure as Measure,
+        action: 'update',
+      });
+    }
+  }
+
+  async onEditCompletion(measure: Measure): Promise<void> {
+    const dialogRef =
+      this._completionDialogService.openCompletionDialog(measure);
+    const updatedMeasure = await firstValueFrom(dialogRef.afterClosed());
+    if (updatedMeasure) {
+      this._changesSubject.next({ item: updatedMeasure, action: 'update' });
+    }
+  }
+
+  async onEditVerification(measure: Measure): Promise<void> {
+    const dialogRef =
+      this._verificationDialogService.openVerificationDialog(measure);
+    const updatedMeasure = await firstValueFrom(dialogRef.afterClosed());
+    if (updatedMeasure) {
+      this._changesSubject.next({ item: updatedMeasure, action: 'update' });
+    }
+  }
+
+  async onDeleteMeasure(measure: Measure): Promise<void> {
+    const confirmDialogRef = this._confirmDialogService.openConfirmDialog(
+      'Delete Measure',
+      `Do you really want to delete measure "${measure.summary}"?`
+    );
+    const confirmed = await firstValueFrom(confirmDialogRef.afterClosed());
+    if (confirmed) {
+      await firstValueFrom(this._measureService.deleteMeasure(measure.id));
+      this._changesSubject.next({ item: measure, action: 'delete' });
+    }
+  }
 }
