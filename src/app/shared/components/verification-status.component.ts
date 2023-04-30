980c597e6a78a9c218df4eccffc01eb46bdf256f
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-04-30 09:43:02 +0200

    add menu to choose verification status to VerificationStatusComponent

--- src/app/shared/components/verification-status.component.ts
+++ src/app/shared/components/verification-status.component.ts
@@ -14,8 +14,10 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Component, Input } from '@angular/core';
-import { Measure } from '../services/measure.service';
+import { Measure, VerificationStatus } from '../services/measure.service';
 import { MeasureInteractionService } from '../services/measure-interaction.service';
+import { VerificationStatusOptions } from '../data/custom/custom-options';
+import { OptionValue } from '../data/options';
 
 @Component({
   selector: 'mvtool-verification-status',
@@ -23,23 +25,47 @@ import { MeasureInteractionService } from '../services/measure-interaction.servi
     <div class="indicator">
       <button
         mat-button
+        [matMenuTriggerFor]="menu"
         [color]="measure.verificationStatusColor"
-        (click)="
-          measureInteractions.onEditVerification(measure);
-          $event.stopImmediatePropagation()
-        "
+        (click)="$event.stopImmediatePropagation()"
         matTooltip="Click to edit verification status"
       >
         <mat-icon *ngIf="measure.verified">check</mat-icon>
         <mat-icon *ngIf="!measure.verified">close</mat-icon>
         {{ measure.verification_status ?? 'not set' | titlecase }}
       </button>
+      <mat-menu #menu="matMenu">
+        <button
+          mat-menu-item
+          *ngFor="
+            let option of verificationStatusOptions.filterOptions() | async
+          "
+          (click)="onSetVerificationStatus(measure, option.value)"
+        >
+          {{ option.label }}
+        </button>
+        <mat-divider></mat-divider>
+        <button
+          mat-menu-item
+          (click)="measureInteractions.onEditVerification(measure)"
+        >
+          Edit Verification
+        </button>
+      </mat-menu>
     </div>
   `,
   styles: [],
 })
 export class VerificationStatusComponent {
   @Input() measure!: Measure;
+  verificationStatusOptions = new VerificationStatusOptions(false);
 
   constructor(readonly measureInteractions: MeasureInteractionService) {}
+
+  onSetVerificationStatus(measure: Measure, value: OptionValue) {
+    this.measureInteractions.onSetVerificationStatus(
+      measure,
+      value as VerificationStatus
+    );
+  }
 }
