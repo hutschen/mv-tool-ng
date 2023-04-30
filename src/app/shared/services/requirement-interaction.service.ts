23a400a55b2ff347dee28598c2e4371ec3cc77fb
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-04-30 11:13:29 +0200

    implement RequirementInteractionService

--- src/app/shared/services/requirement-interaction.service.ts
+++ src/app/shared/services/requirement-interaction.service.ts
@@ -14,10 +14,78 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Injectable } from '@angular/core';
+import { Interaction, InteractionService } from '../data/interaction';
+import { Requirement, RequirementService } from './requirement.service';
+import { Subject, firstValueFrom } from 'rxjs';
+import { RequirementDialogService } from 'src/app/requirement/requirement-dialog.component';
+import { ComplianceDialogService } from '../components/compliance-dialog.component';
+import { ConfirmDialogService } from '../components/confirm-dialog.component';
+import { Project } from './project.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RequirementInteractionService {
-  constructor() {}
+export class RequirementInteractionService
+  implements InteractionService<Requirement>
+{
+  protected _interactionsSubject = new Subject<Interaction<Requirement>>();
+  interactions$ = this._interactionsSubject.asObservable();
+
+  constructor(
+    protected _requirementService: RequirementService,
+    protected _requirementDialogService: RequirementDialogService,
+    protected _complianceDialogService: ComplianceDialogService,
+    protected _confirmDialogService: ConfirmDialogService
+  ) {}
+
+  protected async _createOrEditRequirement(
+    project: Project,
+    requirement?: Requirement
+  ): Promise<void> {
+    const dialogRef = this._requirementDialogService.openRequirementDialog(
+      project,
+      requirement
+    );
+    const resultingRequirement = await firstValueFrom(dialogRef.afterClosed());
+    if (resultingRequirement) {
+      this._interactionsSubject.next({
+        item: resultingRequirement,
+        action: requirement ? 'update' : 'create',
+      });
+    }
+  }
+
+  async onCreateRequirement(project: Project): Promise<void> {
+    await this._createOrEditRequirement(project);
+  }
+
+  async onEditRequirement(requirement: Requirement): Promise<void> {
+    await this._createOrEditRequirement(requirement.project, requirement);
+  }
+
+  async onEditCompliance(requirement: Requirement): Promise<void> {
+    const dialogRef =
+      this._complianceDialogService.openComplianceDialog(requirement);
+    const updatedRequirement = await firstValueFrom(dialogRef.afterClosed());
+    if (updatedRequirement) {
+      this._interactionsSubject.next({
+        item: updatedRequirement as Requirement,
+        action: 'update',
+      });
+    }
+  }
+
+  async onDeleteRequirement(requirement: Requirement): Promise<void> {
+    const confirmDialogRef = this._confirmDialogService.openConfirmDialog(
+      'Delete Requirement',
+      `Do you really want to delete requirement "${requirement.summary}"?`
+    );
+    const confirmed = await firstValueFrom(confirmDialogRef.afterClosed());
+    if (confirmed) {
+      await firstValueFrom(
+        this._requirementService.deleteRequirement(requirement.id)
+      );
+      this._interactionsSubject.next({ item: requirement, action: 'delete' });
+    }
+  }
 }
