44162e48d71dcfbd653b79d9ee9af094c90ce4dd
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-04-30 19:21:57 +0200

    generate DocumentInteractionService

---
+++ src/app/shared/services/document-interaction.service.spec.ts
@@ -0,0 +1,31 @@
+// Copyright (C) 2023 Helmar Hutschenreuter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { TestBed } from '@angular/core/testing';
+
+import { DocumentInteractionService } from './document-interaction.service';
+
+describe('DocumentInteractionService', () => {
+  let service: DocumentInteractionService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DocumentInteractionService);
+  });
+
+  xit('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
