8f0db3cd15ce8d4eba50b85c0bfc176c1a8e46bb
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-02 19:41:08 +0200

    implement CatalogModuleInteractionService

--- src/app/shared/services/catalog-module-interaction.service.ts
+++ src/app/shared/services/catalog-module-interaction.service.ts
@@ -14,10 +14,81 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Injectable } from '@angular/core';
+import { CatalogModule, CatalogModuleService } from './catalog-module.service';
+import { Interaction, InteractionService } from '../data/interaction';
+import {
+  Observable,
+  Subject,
+  filter,
+  firstValueFrom,
+  map,
+  startWith,
+} from 'rxjs';
+import { CatalogModuleDialogService } from 'src/app/catalog-module/catalog-module-dialog.component';
+import { ConfirmDialogService } from '../components/confirm-dialog.component';
+import { Catalog } from './catalog.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CatalogModuleInteractionService {
-  constructor() {}
+export class CatalogModuleInteractionService
+  implements InteractionService<CatalogModule>
+{
+  protected _interactionsSubject = new Subject<Interaction<CatalogModule>>();
+  interactions$ = this._interactionsSubject.asObservable();
+
+  constructor(
+    protected _catalogModuleService: CatalogModuleService,
+    protected _catalogModuleDialogService: CatalogModuleDialogService,
+    protected _confirmDialogService: ConfirmDialogService
+  ) {}
+
+  syncCatalogModule(catalogModule: CatalogModule): Observable<CatalogModule> {
+    return this.interactions$.pipe(
+      filter((interaction) => interaction.item.id === catalogModule.id),
+      map((interaction) => interaction.item),
+      startWith(catalogModule)
+    );
+  }
+
+  protected async _createOrEditCatalogModule(
+    catalog: Catalog,
+    catalogModule?: CatalogModule
+  ): Promise<void> {
+    const dialogRef = this._catalogModuleDialogService.openCatalogModuleDialog(
+      catalog,
+      catalogModule
+    );
+    const resultingCatalogModule = await firstValueFrom(
+      dialogRef.afterClosed()
+    );
+    if (resultingCatalogModule) {
+      this._interactionsSubject.next({
+        item: resultingCatalogModule,
+        action: catalogModule ? 'update' : 'create',
+      });
+    }
+  }
+
+  async onCreateCatalogModule(catalog: Catalog): Promise<void> {
+    await this._createOrEditCatalogModule(catalog);
+  }
+
+  async onEditCatalogModule(catalogModule: CatalogModule): Promise<void> {
+    await this._createOrEditCatalogModule(catalogModule.catalog, catalogModule);
+  }
+
+  async onDeleteCatalogModule(catalogModule: CatalogModule): Promise<void> {
+    const confirmDialogRef = this._confirmDialogService.openConfirmDialog(
+      'Delete Catalog Module',
+      `Do you really want to delete the catalog module "${catalogModule.title}"?`
+    );
+    const confirmed = await firstValueFrom(confirmDialogRef.afterClosed());
+    if (confirmed) {
+      await firstValueFrom(
+        this._catalogModuleService.deleteCatalogModule(catalogModule.id)
+      );
+      this._interactionsSubject.next({ item: catalogModule, action: 'delete' });
+    }
+  }
 }
