e8674a100df14626ba3268a915088baed8d7ab81
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-08-20 11:21:34 +0200

    include AutoNumberInputComponent in templates of bulk edit dialogs

--- src/app/catalog-module/catalog-module-bulk-edit-dialog.component.ts
+++ src/app/catalog-module/catalog-module-bulk-edit-dialog.component.ts
@@ -61,6 +61,7 @@ export class CatalogModuleBulkEditDialogService {
   styleUrls: ['../shared/styles/flex.scss'],
   styles: [
     '.checkbox { margin-bottom: 21.5px; }',
+    '.toggle { text-align: right; margin: -8px 0 16px 0;}',
     '.fx-center { align-items: center; }',
   ],
 })
--- src/app/measure/measure-bulk-edit-dialog.component.ts
+++ src/app/measure/measure-bulk-edit-dialog.component.ts
@@ -65,6 +65,7 @@ export class MeasureBulkEditDialogService {
   styleUrls: ['../shared/styles/flex.scss'],
   styles: [
     '.checkbox { margin-bottom: 21.5px; }',
+    '.toggle { text-align: right; margin: -8px 0 16px 0;}',
     '.fx-center { align-items: center; }',
   ],
 })
