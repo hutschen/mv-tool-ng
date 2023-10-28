c3b1cc767728c434d2aa6627bb64f5c5ae236faa
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-10-28 12:05:15 +0200

    implement ControlValueAccessor interface in SelectionListComponent

--- src/app/shared/components/selection-list.component.ts
+++ src/app/shared/components/selection-list.component.ts
@@ -13,51 +13,63 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
 import {
   IOption,
   OptionValue,
   Options,
-  areSelectedValuesChanged,
   fromOptionValues,
   toOptionValues,
 } from '../data/options';
 import {
-  ReplaySubject,
+  Subject,
+  distinctUntilChanged,
   filter,
   finalize,
   map,
+  merge,
+  share,
+  skip,
   switchMap,
   takeUntil,
-  withLatestFrom,
 } from 'rxjs';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'mvtool-selection-list',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectionListComponent),
+      multi: true,
+    },
+  ],
   template: `
     <!-- Loading indicator -->
-    <div class="loading fx-row" *ngIf="isLoadingOptions">
+    <div class="loading fx-row" *ngIf="_isLoadingOptions">
       <div class="fx-grow">Loading ...</div>
       <div><mat-spinner [diameter]="20"></mat-spinner></div>
     </div>
 
     <!-- Selection list -->
     <mat-selection-list
-      *ngIf="!isLoadingOptions"
+      *ngIf="!_isLoadingOptions"
       [multiple]="options.isMultipleSelection"
     >
       <mat-list-option
         *ngIf="options.isMultipleSelection"
         (click)="invertSelection ? toggleSelectNothing() : toggleSelectAll()"
         [selected]="invertSelection ? isNothingSelected : isAllSelected"
+        [disabled]="_isDisabled"
       >
         Select all
       </mat-list-option>
       <mat-list-option
-        *ngFor="let option of allOptions"
+        *ngFor="let option of _loadedOptions"
         [value]="option.value"
         [selected]="options.isSelected(option) !== invertSelection"
         (click)="options.toggleOption(option)"
+        [disabled]="_isDisabled"
       >
         {{ option.label }}
       </mat-list-option>
@@ -68,68 +80,105 @@ import {
     '.loading { line-height: 24px; padding-left: 16px; padding-right: 16px;}',
   ],
 })
-export class SelectionListComponent implements OnInit {
+export class SelectionListComponent implements OnInit, ControlValueAccessor {
   @Input() options!: Options;
   @Input() invertSelection = false;
-  @Output() valueChange = new EventEmitter<any>();
-  protected _valueSubject = new ReplaySubject<OptionValue[]>(1);
+  protected _writtenValueSubject = new Subject<OptionValue[]>();
 
-  allOptions!: IOption[];
-  isLoadingOptions = false;
+  protected _loadedOptions!: IOption[];
+  protected _isLoadingOptions = false;
+
+  // For ControlValueAccessor
+  protected _onChange: (value: any) => void = () => {};
+  protected _onTouched: () => void = () => {};
+  protected _isDisabled = false;
 
   constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     // invertSelection flag can only be set on multiple selections
     if (this.invertSelection && !this.options.isMultipleSelection) {
       throw new Error('invertSelection can only be set on multiple selections');
     }
 
     // Initially load all options
-    this.isLoadingOptions = true && this.options.hasToLoad;
+    this._isLoadingOptions = true && this.options.hasToLoad;
     this.options
       .getAllOptions()
-      .pipe(finalize(() => (this.isLoadingOptions = false)))
-      .subscribe((options) => {
-        this.allOptions = options;
-      });
+      .pipe(finalize(() => (this._isLoadingOptions = false)))
+      .subscribe((options) => (this._loadedOptions = options));
 
-    // Set the incoming value
-    this._valueSubject
+    // Manages the current value. This is necessary because a value written via
+    // writeValue will only actually be reflected in this.options.selection$
+    // after some time, since options corresponding to the value have to be
+    // loaded.
+    const valueChanges$ = merge(
+      this.options.selection$.pipe(
+        map((options) => options.map((o) => o.value)),
+        map((values) => [values, false] as [OptionValue[], boolean])
+      ),
+      this._writtenValueSubject.pipe(
+        map((values) => [values, true] as [OptionValue[], boolean])
+      )
+    ).pipe(
+      // Only emit if the value is changed
+      distinctUntilChanged(
+        ([prev], [curr]) =>
+          prev.length === curr.length &&
+          prev.every(Set.prototype.has, new Set(curr))
+      ),
+      // Skip the first value, since it is the initial value
+      skip(1),
+      share()
+    );
+
+    // Set incoming values which are set using the writeValue method
+    valueChanges$
       .pipe(
-        withLatestFrom(this.options.selection$),
-        // Check if selection will be changed by the new value
-        filter(([values, options]) =>
-          areSelectedValuesChanged(
-            values,
-            options.map((o) => o.value)
-          )
-        ),
+        filter(([, isWritten]) => isWritten),
+        map(([values]) => values),
         // Load the options corresponding to the new value,
         // but stop if the selection changes in the meantime
-        switchMap(([values]) =>
-          this.options
-            .getOptions(...values)
-            .pipe(takeUntil(this.options.selectionChanged$))
-        )
+        switchMap((values) => {
+          this._isLoadingOptions = true && this.options.hasToLoad;
+          return this.options.getOptions(...values).pipe(
+            takeUntil(this.options.selectionChanged$),
+            finalize(() => (this._isLoadingOptions = false))
+          );
+        })
       )
       .subscribe((options) => {
         this.options.setSelection(...options);
       });
 
-    // Emit the valueChange event when the selection changes
-    this.options.selectionChanged$
-      .pipe(map((options) => options.map((o) => o.value)))
+    // Call onChange when the value is changed by the control itself
+    valueChanges$
+      .pipe(
+        filter(([, isWritten]) => !isWritten),
+        map(([values]) => values)
+      )
       .subscribe((values) => {
-        this.valueChange.emit(
+        this._onChange(
           fromOptionValues(values, this.options.isMultipleSelection)
         );
+        this._onTouched();
       });
   }
 
-  @Input()
-  set value(value: unknown) {
-    this._valueSubject.next(toOptionValues(value));
+  writeValue(value: any): void {
+    this._writtenValueSubject.next(toOptionValues(value));
+  }
+
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this._isDisabled = isDisabled;
   }
 
   get isNothingSelected(): boolean {
@@ -137,7 +186,7 @@ export class SelectionListComponent implements OnInit {
   }
 
   get isAllSelected(): boolean {
-    return this.allOptions.length === this.options.selection.length;
+    return this._loadedOptions.length === this.options.selection.length;
   }
 
   toggleSelectNothing(): void | boolean {
@@ -145,7 +194,7 @@ export class SelectionListComponent implements OnInit {
       return false;
     }
     if (this.isNothingSelected) {
-      this.options.setSelection(...this.allOptions);
+      this.options.setSelection(...this._loadedOptions);
     } else {
       this.options.clearSelection();
     }
@@ -158,7 +207,7 @@ export class SelectionListComponent implements OnInit {
     if (this.isAllSelected) {
       this.options.clearSelection();
     } else {
-      this.options.setSelection(...this.allOptions);
+      this.options.setSelection(...this._loadedOptions);
     }
   }
 }
